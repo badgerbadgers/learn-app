a055a9716acbc0fa301bd9a8d081cb4c06015ba6
Rachel Fischoff <rfischoff@gmail.com>
2022-03-01 13:48:43 -0500

    fix: added customized profile in github provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,17 +7,17 @@ import GitHubProvider from "next-auth/providers/github";
 // https://next-auth.js.org/configuration/options
 export default NextAuth({
   // jwt: true,
-  logger: {
-    error(code, metadata) {
-      console.error(code, metadata)
-    },
-    warn(code) {
-      console.warn(code)
-    },
-    debug(code, metadata) {
-      console.debug(code, metadata)
-    }
-  },
+  // logger: {
+  //   error(code, metadata) {
+  //     console.error(code, metadata);
+  //   },
+  //   warn(code) {
+  //     console.warn(code);
+  //   },
+  //   debug(code, metadata) {
+  //     console.debug(code, metadata);
+  //   },
+  // },
   session: {
     // Choose how you want to save the user session.
     // The default is `"jwt"`, an encrypted JWT (JWE) in the session cookie.
@@ -26,55 +26,81 @@ export default NextAuth({
     // When using `"database"`, the session cookie will only contain a `sessionToken` value,
     // which is used to look up the session in the database.
     strategy: "database",
-  
+
     // Seconds - How long until an idle session expires and is no longer valid.
     maxAge: 30 * 24 * 60 * 60, // 30 days
-  
+
     // Seconds - Throttle how frequently to write to database to extend a session.
     // Use it to limit write operations. Set to 0 to always update the database.
     // Note: This option is ignored if using JSON Web Tokens
     updateAge: 24 * 60 * 60, // 24 hours
   },
-  callbacks:{
-  // async session({ session, token, user }) {
-  //   console.debug(user, 'session-user');
-  //   return session
+  // callbacks: {
+  //   // async session({ session, token, user }) {
+  //   //   console.debug(user, 'session-user');
+  //   //   return session
+  //   // },
+  //   async signIn({ user, account, profile, email, credentials }) {
+  
+  //     return true;
+  //   },
+  //   // async jwt({ token, user, account, profile, isNewUser }) {
+  //   //   console.debug(user, 'jwt-user');
+  //   //   console.debug(profile, 'jwt-profile');
+  //   //   return profile
+  //   // }
   // },
-  async signIn({ user, account, profile, email, credentials }) {
-    console.debug(profile, 'profile')
-    return true
-  }
-  // async jwt({ token, user, account, profile, isNewUser }) {
-  //   console.debug(user, 'jwt-user');
-  //   console.debug(profile, 'jwt-profile');
-  //   return profile
-  // }
-},
-// events: {
-//   async signIn(message) { /* on successful sign in */ },
-//   async signOut(message) { /* on signout */ },
-//   async createUser(message) { /* user created */ },
-//   async updateUser(message) { /* user updated - e.g. their email was verified */ },
-//   async linkAccount(message) { /* account (e.g. Twitter) linked to a user */ },
-//   async session(message) { /* session is active */ },
-//   async error(message) { /* error in authentication flow */ }
-// },
+
+  // events: {
+  //   async signIn(message) { /* on successful sign in */ },
+  //   async signOut(message) { /* on signout */ },
+  //   async createUser(message) { /* user created */ },
+  //   async updateUser(message) { /* user updated - e.g. their email was verified */ },
+  //   async linkAccount(message) { /* account (e.g. Twitter) linked to a user */ },
+  //   async session(message) { /* session is active */ },
+  //   async error(message) { /* error in authentication flow */ }
+  // },
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET
-    //   profile(profile) {
-    //     return {
-    //       id: profile.sub,
-    //       name: profile.name,
-    //       email: profile.email,
-    //       image: profile.picture,
-    //       gh: profile.login,
-    //       url: profile.url
-    //     }
-    //     }
-    })
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      userinfo: {
+        url: "https://api.github.com/user",
+        async request({ client, tokens }) {
+          // Get base profile
+          const profile = await client.userinfo(tokens);
+
+          // If user has email hidden, get their primary email from the GitHub API
+          if (!profile.email) {
+            const emails = await (
+              await fetch("https://api.github.com/user/emails", {
+                headers: { Authorization: `token ${tokens.access_token}` },
+              })
+            ).json();
+
+            if (emails?.length > 0) {
+              // Get primary email
+              profile.email = emails.find((email) => email.primary)?.email;
+              // And if for some reason it doesn't exist, just use the first
+              if (!profile.email) profile.email = emails[0].email;
+            }
+          }
+          // console.debug(profile, "userinfo profile");
+          return profile;
+        },
+      },
+        profile(profile) {
+          return {
+            id: profile.id.toString(),
+            name: profile.name,
+            email: profile.email,
+            image: profile.avatar_url,
+            gh: profile.login,
+            url: profile.url
+          }
+          }
+    }),
   ],
   // pages: {
   //   signIn: '/auth/signin',
@@ -83,7 +109,5 @@ export default NextAuth({
   //   verifyRequest: '/auth/verify-request', // (used for check email message)
   //   newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
   // },
-  debug: true
+  // debug: true,
 });
-
-
