9f0ca64b1eeae3c414fa05f8446fdaa4557ff022
Rachel Fischoff <rfischoff@gmail.com>
2022-04-02 19:12:59 -0400

    fix: fixed typo in ThemeContextWrapper.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import ThemeContextWrapper from "../components/theme/ThemeContextWrapper";
-import { ThemeContext, themes } from "../components/theme/thmeContext";
+import { ThemeContext, themes } from "../components/theme/themeContext";
+import PrivateLayout from "../components/PrivateLayout";
+import PublicLayout from "../components/PublicLayout";
 import { Grid, Switch, Typography, Avatar } from "@mui/material";
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
@@ -9,30 +11,38 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const [darkMode, setDarkMode] = useState(true);
 
   return (
-    <SessionProvider session={session}>
-      <ThemeContextWrapper>
-        <ThemeContext.Consumer>
-          {({ changeTheme }) => (
-            <Grid item display="flex" m={4}>
-              <Avatar alt="Code the Deam" src="../img/ctd-logo.png">
-                CD
-              </Avatar>
-              <Switch
-                checked={darkMode}
-                onClick={() => {
-                  setDarkMode(!darkMode);
-                  changeTheme(darkMode ? themes.light : themes.dark);
-                }}
-              />
-              <Typography variant="8" alignSelf="center">
-                {darkMode ? "Dark Mode" : "Light Mode"}
-              </Typography>
-            </Grid>
-          )}
-        </ThemeContext.Consumer>
-        <Component {...pageProps} />
-      </ThemeContextWrapper>
-    </SessionProvider>
+    <ThemeContextWrapper>
+      <ThemeContext.Consumer>
+        {({ changeTheme }) => (
+          <Grid item display="flex" m={4}>
+            <Avatar alt="Code the Deam" src="../img/ctd-logo.png">
+              CD
+            </Avatar>
+            <Switch
+              checked={darkMode}
+              onClick={() => {
+                setDarkMode(!darkMode);
+                changeTheme(darkMode ? themes.light : themes.dark);
+              }}
+            />
+            <Typography variant="8" alignSelf="center">
+              {darkMode ? "Dark Mode" : "Light Mode"}
+            </Typography>
+          </Grid>
+        )}
+      </ThemeContext.Consumer>
+      <SessionProvider session={session}>
+        {props.Component.name === "Cards" ? (
+          <PublicLayout>
+            <Component {...pageProps} />
+          </PublicLayout>
+        ) : (
+          <PrivateLayout>
+            <Component {...pageProps} />
+          </PrivateLayout>
+        )}
+      </SessionProvider>
+    </ThemeContextWrapper>
   );
 }
 
