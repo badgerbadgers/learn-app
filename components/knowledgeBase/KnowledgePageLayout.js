140b8386918ffacd0074d0ed69e44f231f956cf5
Alena <83471057+alenamedved@users.noreply.github.com>
2022-04-05 12:26:55 -0600

    added theme provider to change title font size to smaller

--- components/knowledgeBase/KnowledgePageLayout.js
+++ components/knowledgeBase/KnowledgePageLayout.js
@@ -1,11 +1,20 @@
-import React, { memo } from "react";
-import { Grid, Typography } from "@mui/material";
+import React from "react";
+import {
+  Grid,
+  Typography,
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from "@mui/material";
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 //Create a Layout for the knowledge Pages.
 //researched - memo hook doesn't work here ...?
 const KnowledgePageLayout = ({ title, index, body }) => {
   return (
-    <div style={{ margin: "70px 47px 0px 47px" }}>
+    <div style={{ margin: "70px 30px 0px" }}>
       <Grid container p={0} m={0} sx={{ maxWidth: "1500px", margin: "auto" }}>
         {/* They can see a (div) with Page Title */}
         <Grid
@@ -13,37 +22,39 @@ const KnowledgePageLayout = ({ title, index, body }) => {
           xs={12}
           sx={{
             align: "center",
-            boxShadow: "0 2px 2px -2px #f1f1f2",
+            boxShadow: "0 4px 2px -2px #f1f1f2",
           }}
         >
-          <Typography
-            variant="h2"
-            sx={{
-              textAlign: "center",
-            }}
-          >
-            {title}
-          </Typography>
+          <ThemeProvider theme={theme}>
+            <Typography
+              variant="h2"
+              sx={{
+                textAlign: "center",
+              }}
+            >
+              {title}
+            </Typography>
+          </ThemeProvider>
         </Grid>
         {/* Content Body container (div) which has 2 sub grids 1.Index / Navigation with links 2.Display Body */}
         <Grid
+          item
           container
           sx={{ margin: 0, align: "center", textAlign: "center" }}
         >
           <Grid
             item
             container
-            rote="index container"
             xs={12}
             md={3}
             sx={{
-              borderRight: "1px solid #f1f1f1",
               paddingTop: 4,
+              justifyContent: "center",
             }}
           >
             {index}
           </Grid>
-          <Grid item container role="body container" xs={12} md={9} sx={{paddingTop: 4}}>
+          <Grid item container xs={12} md={9} sx={{ paddingTop: 4 }}>
             {body}
           </Grid>
         </Grid>
