e32e126f93147314e796bfb6345f260810de9f31
Alena <83471057+alenamedved@users.noreply.github.com>
2022-04-07 12:12:06 -0600

    created component Footer, used at PrivateLayout

--- components/PrivateLayout.js
+++ components/PrivateLayout.js
@@ -1,13 +1,15 @@
 import styles from "../styles/Home.module.css";
 import Header from "./Header";
 import NavBar from "./NavBar";
+import Footer from "../Footer";
 
 const PrivateLayout = ({ children }) => {
   return (
     <>
       <Header />
-      <NavBar/>
-       <main className={styles.main}>{children}</main>
+      <NavBar />
+      <main className={styles.main}>{children}</main>
+      <Footer />
     </>
   );
 };
