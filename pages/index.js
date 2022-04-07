20669c6de12429dcd36b56a41202a3c1e319c79d
Rachel Fischoff <rfischoff@gmail.com>
2022-04-07 10:38:18 -0400

    octokit dependency added & example request in github.js

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,36 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import LogIn from "../components/Login";
+import getGitHub from "../lib/github";
 
 export default function Home() {
   const { data: session } = useSession();
+
+  console.log( getGitHub(), 'octokit response');
 
   return (
     <>
       <h1 className="title">Code the Dream Apprentice Landing Page</h1>
       <LogIn></LogIn>
-
       <ul>
         <li>
           <Link href="/">Home</Link>
         </li>
         {session && (
           <>
-          <li>
-            <Link href={`/portfolios/${encodeURIComponent(session.user.gh)}`}>
-              View Portfolio Page
-            </Link>
-          </li>
-          <li>
-            <Link href={`/userform/${encodeURIComponent(session.user.gh)}`}>
-              Edit Portfolio Page
-            </Link>
-          </li>
+            <li>
+              <Link href={`/portfolios/${encodeURIComponent(session.user.gh)}`}>
+                View Portfolio Page
+              </Link>
+            </li>
+            <li>
+              <Link href={`/userform/${encodeURIComponent(session.user.gh)}`}>
+                Edit Portfolio Page
+              </Link>
+            </li>
           </>
         )}
       </ul>
     </>
   );
 }
-
