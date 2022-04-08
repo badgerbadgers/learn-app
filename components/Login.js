1a13ca54a41a586b3238c63479ee041ec977cd65
CodergirlVS <vaidehis1214@gmail.com>
2022-04-07 22:27:39 -0700

    SignIn redirect done

--- components/Login.js
+++ components/Login.js
@@ -1,85 +1,52 @@
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Stack, Button, Typography } from "@mui/material/";
-import { makeStyles } from "@mui/styles";
-
-const useStyles = makeStyles({
-  // font: {
-  //   fontSize: "Gotham Rounded",
-  // },
-  button: {
-    backgroundColor: "#FF5C35",
-    "&:hover": {
-      backgroundColor: "#12284C",
-    },
-  },
-});
 
 export default function LogIn() {
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
   const { data: session, status } = useSession();
-  const [isloggedIn, setIsLoggedIn] = useState(false);
 
-  const classes = useStyles();
-
-  const handleLogin = () => {
-    // if (status === "authenticated") {
-    if (status === "authenticated") {
-      setIsLoggedIn(false);
-      console.log("***** SignOut***  loggedout");
-      // console.log("Signed in as", session.user.name || session.user.gh);
-      signOut();
-    } else {
-      console.log("*******I am waiting for SingIn loggedIn");
-      console.log("Signed in as", session.user.name || session.user.gh);
-      signIn();
-      setIsLoggedIn(true);
-    }
-
-    // }
-  };
-
-  console.log(isloggedIn, "*****loggedIn status");
-
+  const router = useRouter();
   const buttonData = [
     {
-      title: isloggedIn ? "Log-Out" : "Log-In",
+      title: "Log-In",
       onClick: () => {
-        signIn();
+        signIn("github", {
+          callbackUrl: "/knowledge-base/pair-pgr-page",
+        });
       },
     },
     {
       title: "Sign-Up",
       onClick: () => {
-        {
-          `/userform/${encodeURIComponent(session.user.gh)}`;
-        }
+        router.push("/knowledge-base/zones");
       },
     },
   ];
 
   return (
-    <>
-      <Stack spacing={8} mt={6}>
-        {buttonData.map((btn, i) => (
-          <Button
-            key={i}
-            size="large"
-            variant="contained"
-            onClick={btn.onClick}
-            sx={{
-              width: 300,
-              height: 100,
-              backgroundColor: "#FF5C35",
-              "&:hover": {
-                backgroundColor: "#12284C",
-              },
-            }}
-          >
-            <Typography>{btn.title}</Typography>
-          </Button>
-        ))}
-      </Stack>
-    </>
+    <Stack spacing={8} mt={6}>
+      {buttonData.map((btn, i) => (
+        <Button
+          key={i}
+          size="large"
+          variant="contained"
+          onClick={btn.onClick}
+          sx={{
+            padding: "36px 54px",
+            backgroundColor: "#FF5C35",
+            "&:hover": {
+              backgroundColor: "#12284C",
+            },
+          }}
+        >
+          <Typography sx={{ fontFamily: "Gotham Rounded B", fontSize: "2rem" }}>
+            {btn.title}
+          </Typography>
+        </Button>
+      ))}
+    </Stack>
   );
 }
 
@@ -97,4 +64,18 @@ export default function LogIn() {
 //       <button onClick={()=>signIn()}>Sign in</button>
 //     </>
 //   );
+// }
+
+// if (status === "authenticated") {
+// if (status === "authenticated") {
+//   setIsLoggedIn(false);
+//   console.log("***** SignOut***  loggedout");
+//   console.log("Signed in as", session.user.name || session.user.gh);
+//   signOut();
+// } else {
+//   const handleLogin = () => {
+//     console.log("*******I am waiting for SingIn loggedIn");
+//     //console.log("Signed in as", session.user.name || session.user.gh) { callbackUrl: "http://www.google.com" };
+
+//   };
 // }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,48 @@
 import { useSession } from "next-auth/react";
-import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
 import LogIn from "../components/Login";
+import {
+  Typography,
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from "@mui/material";
+
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
   const { data: session } = useSession();
 
   return (
     <>
-      <h1 className="title">Code the Dream Apprentice Landing Page</h1>
-      <LogIn />
+      {/* {isLoading ? (
+        <Image width={240} height={240} src="/img/loading.gif" alt="loading" />
+      ) : ( */}
+      <>
+        <ThemeProvider theme={theme}>
+          <Typography
+            variant="h2"
+            sx={{
+              textAlign: "center",
+              fontFamily: "Gotham Rounded B",
+              fontSize: "42px",
+            }}
+          >
+            Code the Dream Apprentice Landing Page
+          </Typography>
+        </ThemeProvider>
+        <LogIn />
+      </>
+      {/* )} */}
+    </>
+  );
+}
 
-      {/* <ul>
+{
+  /* <ul>
         <li>
           <Link href="/">Home</Link>
         </li>
@@ -28,7 +60,5 @@ export default function Home() {
             </li>
           </>
         )}
-      </ul> */}
-    </>
-  );
+      </ul> */
 }
