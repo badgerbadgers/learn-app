fa28aed6c9ef0e9463f5eb741d32cd7fe26944f9
Rachel Fischoff <rfischoff@gmail.com>
2022-04-11 15:18:56 -0400

    fix: created createUser and updateUser functions in api/users

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,5 +1,5 @@
 import { getSession } from "next-auth/react";
-import  clientPromise  from "../../lib/mongodb";
+import clientPromise from "../../lib/mongodb";
 import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
@@ -19,11 +19,13 @@ export default async function handler(req, res) {
 const getUser = async (res, req) => {
   // connect to database
   const client = await clientPromise;
-  const database = client.db(process.env.MONGODB_DB)
+  const database = client.db(process.env.MONGODB_DB);
 
   // find the document matching the query.id - github id
   try {
-    let doc = await database.collection("usersprofile").findOne({ gh: req.query.id });
+    let doc = await database
+      .collection("usersprofile")
+      .findOne({ gh: req.query.id });
     return res.status(200).json(doc);
   } catch (error) {
     console.log(error, "error from getUser in /api/users");
@@ -32,9 +34,7 @@ const getUser = async (res, req) => {
 
 // const createNewUser & updateUser
 
-
-const updateUser = async (req, res) => {
-
+const createNewUser = async (req, req) => {
   //connect to database
   const client = await clientPromise;
   const database = client.db(process.env.MONGODB_DB);
@@ -54,13 +54,38 @@ const updateUser = async (req, res) => {
   data["createdAt"] = new Date();
 
   try {
-    await database.collection("usersprofile").findOneAndUpdate(
-      { gh: req.query.id },
-      { $set: data },
-      { upsert: true },
-      // thought the statement below would add last modified doesn't seem too
-      { $currentDate: { lastModified: true } }
-    );
+    await database
+      .collection("usersprofile")
+      .findOneAndUpdate({ gh: req.query.id }, { $set: data }, { upsert: true });
+    res.status(200).json({ message: `updated User ${req.query.id}` });
+  } catch (error) {
+    console.log(error, "error from updateUser in api/users");
+  }
+};
+
+const updateUser = async (req, res) => {
+  //connect to database
+  const client = await clientPromise;
+  const database = client.db(process.env.MONGODB_DB);
+
+  //get user ObjectId and image from session
+  const session = await getSession({ req });
+  // const userId = session.user.id;
+  // const image = session.user.image;
+
+  //data object from submit form
+  let data = req.body.body;
+
+  data = JSON.parse(data);
+  data["gh"] = req.query.id;
+  // data["userId"] = ObjectId(userId);
+  // data["image"] = image;
+  data["updatedAt"] = new Date();
+
+  try {
+    await database
+      .collection("usersprofile")
+      .findOneAndUpdate({ gh: req.query.id }, { $set: data }, { upsert: true });
     res.status(200).json({ message: `updated User ${req.query.id}` });
   } catch (error) {
     console.log(error, "error from updateUser in api/users");
