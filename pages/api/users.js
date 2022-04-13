7afe0539f6ed50b9e8fa5fc7fb36560ad4b16dfe
Rachel Fischoff <rfischoff@gmail.com>
2022-04-13 14:49:51 -0400

    fix: refactored and updated createAndUpdateUser in users api route

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,15 +1,7 @@
-// do i need this or can i add to user object on client side - feels like an extra network call 
-import { getSession } from "next-auth/react"; 
+// do i need this or can i add to user object on client side - feels like an extra network call
+import { getSession } from "next-auth/react";
 import clientPromise from "../../lib/mongodb";
 import { ObjectId } from "mongodb";
-
-
-/* 
-1) make conditional of POST (updateUser v createUser)
-2) add error handling if doc is null or id is undefined to functions
-3) remove getSession calls and add the data on the client-side 
-4) merge document for updateUser
-*/
 
 export default async function handler(req, res) {
   const { method } = req;
@@ -18,8 +10,7 @@ export default async function handler(req, res) {
     case "GET":
       return getUser(req, res);
     case "POST":
-      //if (req.query.isEdit) { updateUser(req, res) } else createUser(res, req)
-      return updateUser(req, res);
+      return createAndUpdateUser(req, res);
     default:
       res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
       res.status(405).end(`Method ${method} Not Allowed`);
@@ -42,9 +33,8 @@ const getUser = async (req, res) => {
   }
 };
 
-// const createNewUser & updateUser
 
-const createUser = async (req, res) => {
+const createAndUpdateUser = async (req, res) => {
   //connect to database
   const client = await clientPromise;
   const database = client.db(process.env.MONGODB_DB);
@@ -61,43 +51,18 @@ const createUser = async (req, res) => {
   data["gh"] = req.query.id;
   data["userId"] = ObjectId(userId);
   data["image"] = image;
-  data["createdAt"] = new Date();
-
-  try {
-    await database
-      .collection("usersprofile")
-      .findOneAndUpdate({ gh: req.query.id }, { $set: data }, { upsert: true });
-    res.status(200).json({ message: `updated User ${req.query.id}` });
-  } catch (error) {
-    console.log(error, "error from updateUser in api/users");
+  if (data["createAt"]) {
+    data["updatedAt"] = new Date();
+  } else {
+    data["createdAt"] = new Date();
   }
-};
-
-const updateUser = async (req, res) => {
-  //connect to database
-  const client = await clientPromise;
-  const database = client.db(process.env.MONGODB_DB);
-
-  //get user ObjectId and image from session
-  const session = await getSession({ req });
-  const userId = session.user.id;
-  const image = session.user.image;
-
-  //data object from submit form
-  let data = req.body.body;
-
-  data = JSON.parse(data);
-  data["gh"] = req.query.id;
-  data["userId"] = ObjectId(userId);
-  data["image"] = image;
-  data["updatedAt"] = new Date();
 
   try {
     await database
       .collection("usersprofile")
       .findOneAndUpdate({ gh: req.query.id }, { $set: data }, { upsert: true });
-    res.status(200).json({ message: `updated User ${req.query.id}` });
+    res.status(200).json({ message: `create and update User ${req.query.id}` });
   } catch (error) {
-    console.log(error, "error from updateUser in api/users");
+    console.log(error, "error from createAndUpdateUser in api/users");
   }
 };
