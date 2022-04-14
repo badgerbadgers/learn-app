f34244ccef61a35838c6769d392610e45656df3f
Muginga <74222775+muginga72@users.noreply.github.com>
2022-04-14 15:51:29 -0400

    Added responsivenes

--- pages/resources/[id]/componentes/ResourceCards.js
+++ pages/resources/[id]/componentes/ResourceCards.js
@@ -11,60 +11,66 @@ import { makeStyles } from "@mui/styles";
 
 // Setting the styles on the root element of ResourceCard component
 const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: 200,
+  },
   button: {
     textTransform: "none",
     borderRadius: "30px",
     marginLeft: "10px",
     marginBottom: "8px",
   },
-  cardContentArea: {
-    background: "#CDDC39",
-  },
+  // cardContentArea: {
+  //   background: "#CDDC39",
+  // },
 }));
 
 function ResourceCards() {
   const classes = useStyles();
 
   return (
-      <Grid item display="flex" m={2}>
-        <Paper elevation={15}>
+    <Grid item display="flex">
+      <Paper elevation={15}>
         <Card
-          sx={{ maxWidth: 300 }}
+        // sx={{ maxWidth: 300 }}
         >
-            {/* CardActionArea's button will allow users to interact with the entirety 
+          {/* CardActionArea's button will allow users to interact with the entirety 
               of its surface to trigger its main action */}
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="300"
-                image="../img/Kodayi-temple.jpg"
-                alt="Temple"
-              />
-            </CardActionArea>
-            <Paper className={classes.cardContentArea}>
-              <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
-                  Coding style guige (Google)
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Coding style/conventional that Google developers want you to
-                  have/follow.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  size="small"
-                  color="primary"
-                  variant="outlined"
-                  className={classes.button}
-                >
-                  JavaScript
-                </Button>
-              </CardActions>
-            </Paper>
-          </Card>
-        </Paper>
-      </Grid>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="300"
+              image="../img/Kodayi-temple.jpg"
+              alt="Temple"
+            />
+          </CardActionArea>
+          <Paper
+            // className={classes.cardContentArea}
+            sx={{ background: "#CDDC39" }}
+          >
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                Coding style guige (Google)
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Coding style/conventional that Google developers want you to
+                have/follow.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button
+                size="small"
+                color="primary"
+                variant="outlined"
+                className={classes.button}
+              >
+                JavaScript
+              </Button>
+            </CardActions>
+          </Paper>
+        </Card>
+      </Paper>
+    </Grid>
   );
 }
 
--- pages/resources/[id]/index.js
+++ pages/resources/[id]/index.js
@@ -1,24 +1,39 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import ResourceCards from "./componentes/ResourceCards";
-import styles from "../../../styles/Resources.module.css";
+import { makeStyles } from "@mui/styles";
 
+// Setting the styles on the root element of ResourceCard component
+const useStyles = makeStyles((theme) => ({
+  griContainer: {
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    marginTop: "5px",
+  }
+}));
 function Resources() {
+  const classes = useStyles();
   return (
-    <Grid container className="main-card-wrapper">
-      <Grid item display="flex" xs={12}>
-        {/* <div className={styles.resourcesGrid}>
-          <div className={styles.resourcesItem}>
-            <div className={styles.innerCard}> */}
-              <ResourceCards />
-              <ResourceCards />
-              <ResourceCards />
-              <ResourceCards />
-              <ResourceCards />
-              <ResourceCards />
-            {/* </div>
-          </div>
-        </div> */}
+    <Grid
+      container spacing={2}
+      className={classes.griContainer}
+      justifyContent="center"
+    >
+      {/* 
+      xs=12 Each card will take entire row, and sm=6 will be two cards in row
+      md=3 will be displayed 4 cards in one row.
+      */}
+      <Grid item xs={12} sm={6} md={3}>
+        <ResourceCards />
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <ResourceCards />
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <ResourceCards />
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <ResourceCards />
       </Grid>
     </Grid>
   );
