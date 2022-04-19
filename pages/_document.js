6dd120156d8f3a8d085b23f3b27b07670e6e7580
Rachel Fischoff <rfischoff@gmail.com>
2022-04-19 15:56:54 -0400

    Feat: getStaticProps implemented in zones index.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,14 @@
-import Document,{ Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function MyDocument() {
   return (
     <Html>
       <Head>
+      <meta name="description" content="Created by Code the Dream Labs" />
+      <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"/>
       </Head>
       <body>
