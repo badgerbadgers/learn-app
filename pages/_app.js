3a3a7b801792ab06016db71df5162fa4e7c1f278
Rachel Fischoff <rfischoff@gmail.com>
2022-04-21 20:19:49 -0400

    added protected.js, admin, and Auth to _app.js for protected routes

--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,18 @@ import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  
   return (
-    <SessionProvider session={session} >
-    <ThemeContextWrapper>
-        {(Component.displayName === "Portfolio" || Component.displayName === "Home") ? (
+    <SessionProvider session={session}>
+      <ThemeContextWrapper>
+        {(Component.displayName === "Portfolio" ||
+          Component.displayName === "Home") ? (
           <PublicLayout>
             <Component {...pageProps} />
           </PublicLayout>
         ) : (
-          <PrivateLayout>
-            <Component {...pageProps} />
-          </PrivateLayout>
+            <PrivateLayout>
+              <Component {...pageProps} />
+            </PrivateLayout>
         )}
       </ThemeContextWrapper>
     </SessionProvider>
@@ -24,3 +24,14 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 }
 
 export default MyApp;
+
+function Auth({ children }) {
+  // if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
+  const { status } = useSession({ required: true });
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return children;
+}
