e97c07b8a907d3f5eb3c7fa5a32f9b0fa3f4d8b3
Alena <83471057+alenamedved@users.noreply.github.com>
2022-04-28 12:14:37 -0600

    added some changes accordingly to lighthouse releted to pwa

--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function MyDocument() {
   return (
-    <Html>
+    <Html lang='en'>
       <Head>
      
       <meta name="description" content="Created by Code the Dream Labs" />
+      <meta name="theme-color" content="#12284C"></meta>
       <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
       <link rel="manifest" href="/manifest.json" />
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
