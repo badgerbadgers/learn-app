2caaace8cc95bfcb5b2d8db70aac8c9f930e9944
CodergirlVS <vaidehis1214@gmail.com>
2022-05-02 16:10:09 -0700

    Created a layout as per wireframes and close issue# 146

--- pages/myprojects/components/ProjectHeader.js
+++ pages/myprojects/components/ProjectHeader.js
@@ -1,9 +1,18 @@
 import FolderSharedRoundedIcon from '@mui/icons-material/FolderSharedRounded';
-import { Typography } from "@mui/material";
+import { Typography, Grid } from "@mui/material";
 
 const ProjectHeader = () => {
   return (
-    <Typography variant="h3" gutterBottom color="secondary">
+    <Grid
+    item
+    xs={12}
+    justify='center'
+    sx={{
+      align: "center",
+      boxShadow: "0 4px 2px -2px #f1f1f2",
+    }}
+    >
+    <Typography variant="h3" gutterBottom color="secondary" textAlign="center">
       <FolderSharedRoundedIcon
         color="secondary"
         style={{
@@ -14,6 +23,7 @@ const ProjectHeader = () => {
       />{" "}
       My Projects
     </Typography>
+    </Grid>
   );
 };
 
--- pages/myprojects/index.js
+++ pages/myprojects/index.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
 import ProjectHeader from "./components/ProjectHeader";
+import ProjectCards from "./components/ProjectCards";
+import { Grid } from "@mui/material";
 
 const MyProjects = () => {
-  return <ProjectHeader />;
+  return (
+    <Grid
+      container
+      xs={12}
+      justify="center"
+      sx={{ maxWidth: "1250px", margin: "auto" }}
+    >
+      <ProjectHeader />
+      <ProjectCards />
+    </Grid>
+  );
 };
 
 export default MyProjects;
