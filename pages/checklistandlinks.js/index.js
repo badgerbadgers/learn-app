f4407c36cb29d075d0ca1c93dbd1533d69f4d1a3
Alena <83471057+alenamedved@users.noreply.github.com>
2022-05-03 15:11:56 -0600

    wip some layouts added

--- pages/checklistandlinks.js/index.js
+++ pages/checklistandlinks.js/index.js
@@ -1,21 +1,34 @@
-
+import { Container, Paper, Grid } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import PrivateLayout from "../../components/PrivateLayout";
 
-
 export default function ChecklistPage() {
+  //use a query to adjust mobile view
+  const matches = useMediaQuery("(min-width:600px)");
   return (
-    <>
-
-    </>
+  <Container sx={{ textAlign: "center", p: !matches && 1 }}>
+<Paper
+      sx={{
+        minWidth: 330,
+        backgroundColor: "background.dbpaper",
+        my: 2,
+        mx: "auto",
+        p: matches ? 8 : 1,
+      }}
+    >
+      <Grid
+        container
+        rowSpacing={2}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        sx={{ justifyContent: "center" }}
+      >
+        {'children'} 
+      </Grid>
+    </Paper>
+  </Container>
   );
 }
 
 ChecklistPage.getLayout = function getLayout(page) {
-    return (
-      <PrivateLayout>
-        {page}
-      </PrivateLayout>
-    )
-  }
-
-
+  return <PrivateLayout>{page}</PrivateLayout>;
+};
