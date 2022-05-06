87eee081437d51f560843b9c445197d69ac3e525
Alena <83471057+alenamedved@users.noreply.github.com>
2022-05-06 09:12:44 -0600

    rerurned old code

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-import { getSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { getSession, useSession } from "next-auth/react";
 import LogIn from "../components/Login";
 import { Typography } from "@mui/material";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import ImageWall from "../components/ImageWall";
 
-export default function Home() {
+export default function Home(props) {
+  const { data: session } = useSession();
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session) {
+      router.push('/dashboard')
+    }
+  }, [])
+
   return (
     <>
       {/* <ImageWall />  */}
@@ -31,6 +42,7 @@ export async function getServerSideProps(context) {
   return {
     props: {
       session: await getSession(context),
+   
     },
   };
 }
