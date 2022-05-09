db09eb94a406aa2ab318ea553fe019eb9959c43e
Alena <83471057+alenamedved@users.noreply.github.com>
2022-05-09 10:56:55 -0600

    moved to layout

--- pages/checklistandlinks/index.js
+++ pages/checklistandlinks/index.js
@@ -1,52 +1,23 @@
-import {
-  Container,
-  Paper,
-  Grid,
-} from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import PrivateLayout from "../../components/PrivateLayout";
+import {privateLayout} from "../../components/PrivateLayout";
 
 import CheckListCard from "./components/CheckListCard";
 import WeeklyMeetingsCard from "./components/WeeklyMeetingsCard";
-
-//import DOMPurify from "isomorphic-dompurify";
-import styles from "../../styles/Knowledge.module.css";
 import CTDAdminCard from "./components/CTDAdminCard";
 import CTDMissionCard from "./components/CTDMissionCard";
+import CheckListLayout from "./components/CheckListLayout";
 
 export default function ChecklistPage() {
   //use a query to adjust mobile view
   const matches = useMediaQuery("(min-width:900px)");
   return (
-    //layout
-    <Container
-    role="container"
-      className={styles.checklistLink}
-      sx={{ textAlign: "center", p: !matches && 1 }}
-    >
-      <Paper
-        sx={{
-          backgroundColor: "background.dbpaper",
-          p: matches ? 8 : 2,
-        }}
-      >
-        <Grid
-          container
-          rowSpacing={2}
-          m={0}
-          columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          sx={{ justifyContent: "center" }}
-        >
-          <CheckListCard />
-          <WeeklyMeetingsCard />
-          <CTDAdminCard />
-          <CTDMissionCard />
-        </Grid>
-      </Paper>
-    </Container>
+    <CheckListLayout matches={matches}>
+      <CheckListCard />
+      <WeeklyMeetingsCard />
+      <CTDAdminCard />
+      <CTDMissionCard />
+    </CheckListLayout>
   );
 }
 
-ChecklistPage.getLayout = function getLayout(page) {
-  return <PrivateLayout>{page}</PrivateLayout>;
-};
+ChecklistPage.getLayout = privateLayout
