c98cc5585e8a28344fe025e74b326a06bb329e3a
hifilorau <tomrau@gmail.com>
2022-05-10 09:08:14 -0400

    WIP added more state to handle filter and shorted searchResults function, added useEffect

--- pages/knowledge-base/resources/index.js
+++ pages/knowledge-base/resources/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Grid } from "@mui/material";
 import ResourceCard from "./components/ResourceCard";
 import minifyItems from "../../../lib/minifyItems";
@@ -7,6 +7,9 @@ import { getResourceData } from "../../../lib/airtable";
 
 function Resources({ resources }) {
   const [searchTerm, setSearchTerm] = useState("");
+  const [activeResources, setActiveResources] = useState(resources);
+  const [filterTerm, setFilterTerm] = useState("")
+
 
   // We want a function that we can search by: name, type, topic, language, and description
   // Create a function that save in array the results temporary the element
@@ -16,53 +19,29 @@ function Resources({ resources }) {
   // changing that string values into lowercase using the toLowerCase method
   // Remove duplicates before pushing to tempResults
   // The return will be the filtered array
-
   const searchResults = (term) => {
-    const tempResults = [];
-    let filteredResults = resources.filter((item) => {
-      // Change string value into lowercase and check if this term exist and save it
-      // or if doesn't exist return empty array
-      const lowerCaseName = item.fields.Name
-        ? item.fields.Name.toLowerCase()
-        : [];
-      const lowerCaseType = item.fields.Type
-        ? item.fields.Type.toLowerCase()
-        : [];
-      const lowerCaseDescription = item.fields.Description
-        ? item.fields.Description.toLowerCase()
-        : [];
-      let topicSearchResults = item.fields["Name (from topic)"]
-        ? item.fields["Name (from topic)"].map((topic) => topic.toLowerCase())
-        : [];
-      let languageSearchResults = item.fields["Name (from language)"]
-        ? item.fields["Name (from language)"].map((topic) =>
-            topic.toLowerCase()
-          )
-        : [];
-
-      return (
-        // Check if exist and change the search term into lowercase
-        lowerCaseName.includes(term.toLowerCase()) ||
-        lowerCaseType.includes(term.toLowerCase()) ||
-        lowerCaseDescription.includes(term.toLowerCase()) ||
-        (topicSearchResults &&
-          topicSearchResults.includes(term.toLowerCase())) ||
-        (languageSearchResults &&
-          languageSearchResults.includes(term.toLowerCase()))
-      );
-    });
-
-    tempResults.push(...filteredResults);
-
-    // Remove duplicates resources before pushing to tempResults
-    // Will push all to the temporary result array and return it
-    const removeDuplicateResources = tempResults.filter((item, id) => {
-      return tempResults.indexOf(item) === id;
-    });
-    return removeDuplicateResources;
+    return resources.filter(item => {
+      const itemName = item.fields.Name ? item.fields.Name.toLowerCase() : ""
+      const itemDescription = item.fields.Description ? item.fields.Description.toLowerCase() : ""
+      const itemType = item.fields.Type ? item.fields.Type.toLowerCase() : ""
+      const topicSearchResults = item.fields["Name (from topic)"]
+      ? item.fields["Name (from topic)"].map((topic) => topic.toLowerCase())
+      : [];
+      const languageSearchResults = item.fields["Name (from language)"]
+      ? item.fields["Name (from language)"].map((topic) =>
+          topic.toLowerCase()) : [];
+        return languageSearchResults.includes(term.toLowerCase()) || topicSearchResults.includes(term.toLowerCase()) || itemName.includes(term.toLowerCase()) || itemDescription.includes(term.toLowerCase()) || itemType.includes(term.toLowerCase())
+    })
   };
 
-  const filteredList = searchResults(searchTerm)
+  useEffect(() => {
+    if (searchTerm) {
+      const newList = searchResults(searchTerm)
+      setActiveResources(newList)
+    }
+  }, [searchTerm])
+
+  // const filteredList = searchResults(searchTerm)
 
   return (
     <Grid
@@ -79,6 +58,8 @@ function Resources({ resources }) {
       <ResourceToolBar
         resources={resources}
         searchTerm={searchTerm}
+        filterTerm={filterTerm}
+        setFilterTerm={setFilterTerm}
         setSearchTerm={setSearchTerm}
       />
       <Grid item xs={12} marginLeft="25px" color="blue">
@@ -90,8 +71,8 @@ function Resources({ resources }) {
         If it is false, the program will ignore and skip it. 
       */}
 
-      {filteredList &&
-        filteredList.map((resource) => {
+      {activeResources &&
+        activeResources.map((resource) => {
           return <ResourceCard key={resource.id} resource={resource} />;
         })}
     </Grid>
