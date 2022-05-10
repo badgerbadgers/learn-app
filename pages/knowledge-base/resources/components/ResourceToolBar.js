7535ce369e4d9b21b6c48b673102c37a9477118f
Muginga <74222775+muginga72@users.noreply.github.com>
2022-05-10 10:14:27 -0400

    Housekeeping the components for PR

--- pages/knowledge-base/resources/components/ResourceToolBar.js
+++ pages/knowledge-base/resources/components/ResourceToolBar.js
@@ -1,52 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import AppBar from "@mui/material/AppBar";
-import { Grid, Stack, Divider } from "@mui/material";
+import { Grid, Stack } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
-import Box from "@mui/material/Box";
-import FormLabel from "@mui/material/FormLabel";
-import FormControl from "@mui/material/FormControl";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import { AddBoxOutlined, AddCircleOutlineOutlined } from "@mui/icons-material";
 import SearchIcon from "@mui/icons-material/Search";
 import {
   Search,
   SearchIconWrapper,
   StyledInputBase,
-  StyledMenu,
 } from "../../../../lib/searchUtils";
 
 // The code starts here
 function ResourceToolBar({
-  resources,
   searchTerm,
   setSearchTerm,
-  setFilterType,
-  isCheckedAll,
 }) {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
-
-  // Events handlers to anchor the menu
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   // *** SEARCH SECTION START HERE ***
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
-
-  // *** FILTERS SECTION START HERE ***
-  // destructuring the state filter by type and filter response
-  // const { cheatsheet, coding, concepts, docs, exercises, other } = filterByType;
 
   return (
     <Grid item xs={12} sx={{ flexGrow: 1 }}>
@@ -74,132 +46,7 @@ function ResourceToolBar({
               justifyContent: "space-between",
               flexWrap: "nowrap",
             }}
-          >
-            <div>
-              <Button
-                id="demo-customized-button"
-                aria-controls={open ? "demo-customized-menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-                variant="contained"
-                disableElevation
-                onClick={handleClick}
-                endIcon={<KeyboardArrowDownIcon />}
-              >
-                Type
-              </Button>
-              <StyledMenu
-                id="demo-customized-menu"
-                MenuListProps={{
-                  "aria-labelledby": "demo-customized-button",
-                }}
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose} disableRipple>
-                  <Box sx={{ display: "flex" }}>
-                    <FormControl
-                      sx={{ m: 2 }}
-                      component="fieldset"
-                      variant="standard"
-                    >
-                      <FormLabel component="legend">
-                        Type of Resources
-                      </FormLabel>
-                      <Divider />
-                      {/* 
-                        Adding multiple checkboxes with FormGroup
-                        Add a label to checkbox with the FormControlLabel to select All
-                      */}
-                      <FormGroup>
-                        {/* <FormControlLabel
-                          control={
-                            <Checkbox
-                              id="selectAll"
-                              name="selectAll"
-                              checked={isCheckedAll}
-                              onChange={(e) => {
-                                setSearchTerm(e.target.name);
-                                setFilterType(e.target.value);
-                              }}
-                            />
-                          }
-                          label="Select All"
-                        >
-                          {resources}
-                        </FormControlLabel>
-                        {/* Add a label to checkbox with the FormControlLabel for single selection */}
-                        {/* <FormControlLabel
-                          control={
-                            <Checkbox
-                              checked={cheatsheet}
-                              onChange={(e) => {
-                                setSearchTerm(e.target.name);
-                                setFilterType(e.target.value);
-                              }}
-                              name="cheatsheet"
-                            />
-                          }
-                          label="Cheatsheet"
-                        />
-                        <FormControlLabel
-                          control={
-                            <Checkbox
-                              checked={coding}
-                              onChange={handleFilterByTypeChange}
-                              name="coding"
-                            />
-                          }
-                          label="Coding"
-                        />
-                        <FormControlLabel
-                          control={
-                            <Checkbox
-                              checked={concepts}
-                              onChange={handleFilterByTypeChange}
-                              name="concepts"
-                            />
-                          }
-                          label="Concepts"
-                        />
-                        <FormControlLabel
-                          control={
-                            <Checkbox
-                              checked={docs}
-                              onChange={handleFilterByTypeChange}
-                              name="docs"
-                            />
-                          }
-                          label="Docs"
-                        />
-                        <FormControlLabel
-                          control={
-                            <Checkbox
-                              checked={exercises}
-                              onChange={handleFilterByTypeChange}
-                              name="exercises"
-                            />
-                          }
-                          label="Exercises"
-                        />
-                        <FormControlLabel
-                          control={
-                            <Checkbox
-                              checked={other}
-                              onChange={handleFilterByTypeChange}
-                              name="other"
-                            />
-                          }
-                          label="Other"
-                        />  */}
-                      </FormGroup>
-                    </FormControl>
-                  </Box>
-                </MenuItem>
-              </StyledMenu>
-            </div>
-          </Stack>
+          />
         </Toolbar>
       </AppBar>
     </Grid>
