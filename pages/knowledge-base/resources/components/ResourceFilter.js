a76e3ee9b078a5b8b69e9ba9caed420dd43559ee
Olena Suvorova <olena@codethedream.org>
2022-07-01 10:13:26 -0700

    Add render value to filters based on selected items.

--- pages/knowledge-base/resources/components/ResourceFilter.js
+++ pages/knowledge-base/resources/components/ResourceFilter.js
@@ -12,7 +12,9 @@ const ITEM_HEIGHT = 40;
 const ITEM_PADDING_TOP = 8;
 
 export const MultipleSelect = ({ items, selectedItems, setSelectedItems, label }) => {
+  const filterStyle = styled => {
 
+  }
   const handleChange = (event) => {
     const value = event.target.value;
     setSelectedItems(
@@ -20,6 +22,17 @@ export const MultipleSelect = ({ items, selectedItems, setSelectedItems, label }
       typeof value === 'string' ? value.split(',') : value,
     );
   };
+
+  const _renderValue = (selected, label) => {
+    if (!selected) return label;
+    if (selected.length == 1) {
+      return selected[0];
+    } else {
+      return selected[0] + `+${selected.length-1}`;
+    }
+
+  }
+  
   return (
       <div>
       <FormControl sx={{ m: 1, width: 200, minWidth: 100 }} size="small">
@@ -30,7 +43,7 @@ export const MultipleSelect = ({ items, selectedItems, setSelectedItems, label }
           value={selectedItems}
           onChange={handleChange}
           input={<OutlinedInput label="Tag" />}
-          renderValue={(selected) => label}
+          renderValue={(selected) => _renderValue(selected, label)}
         >
           
           {items.map((name) => (
