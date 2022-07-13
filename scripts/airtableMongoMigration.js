a954086ac4b761c3bad3ab89c5f3cc939c7374cc
Olena Suvorova <olena@codethedream.org>
2022-07-13 15:52:48 -0700

    WIP: Fix data source.

--- scripts/airtableMongoMigration.js
+++ scripts/airtableMongoMigration.js
@@ -17,15 +17,13 @@ const insertToMongo = async (doc, coll) => {
     // @param {string} collection in Mongo
 
     const uri = await getConfigParam(/^MONGODB_URI=(.+)/);
-    console.log(uri, 'uri to mongo');
     const client = new MongoClient(uri);
     async function run() {
         try {
             await client.connect();
             const database = client.db('myFirstDatabase');
-            console.log(database, "*****database befor insertion");
             const mongoCollectionName = database.collection(coll);
-            const result = await mongoCollectionName.insertOne(doc);
+            const result = await mongoCollectionName.insertOne(doc); // TODO: filter + maybe pass filter to the func + upsert
             console.log(
                 `A document was inserted with the _id: ${result.insertedId} `, //${result.insertedId}
             );
@@ -39,151 +37,40 @@ const insertToMongo = async (doc, coll) => {
 }
 const getDataFromAirtable = async () => {
     const Airtable = require("airtable");
-    const param = await getConfigParam(/AIRTABLE_TEST=(.+)/);
-    const AtBase = new Airtable({ apiKey: param }).base('appXnj8G9efiiY9Ge'); //The ID of Class Mangement base
+    const param = await getConfigParam(/AT_KEY=(.+)/);
 
-    let classesInfo = {}; //Cohorts cache
-    AtBase('Classes').select({
-    }).eachPage(function page(records, fetchNextPage) {
-        records.forEach(function (record) {
-            classesInfo[record.id] = record.fields;
-        });
-        fetchNextPage();
-    }, function done(err) {
-        if (err) { console.error(err); return; }
-    });
+    const AtBase = new Airtable({ apiKey: param }).base('appN9Hf8KluRDBAN4'); //The ID of Class Mangement base
+
+    // let classesInfo = {}; //Cohorts cache
+    // AtBase('Cohorts').select({
+    //     // view: "Grid view"
+    // }).eachPage(function page(records, fetchNextPage) {
+    //     records.forEach(function (record) {
+    //         classesInfo[record.id] = record.fields;
+    //     });
+    //     fetchNextPage();
+    // }, function done(err) {
+    //     if (err) { console.error(err); return; }
+    // });
+
 
     AtBase('Courses').select({
-        view: "Default View",
-
+        // view: "Grid View",
     }).eachPage(function page(records, fetchNextPage) {
         records.forEach(function (record) {
-            let course = {
-                course_name: record.get('Name'),
-                airtable_id: record.id,
+            if (record.get('Name')) { // in case if there is an empty row in the airtable
+                let course = {
+                    course_name: record.get('Name'),
+                    airtable_id: record.id,
+                }
+                insertToMongo(course, 'courses')
             }
-            insertToMongo(course, 'courses')
         });
         fetchNextPage();
 
     }, function done(err) {
         if (err) { console.error(err); return; }
     });
-
-
-
 }
 
 getDataFromAirtable();
-
-
-
-
-
-
-
-
-
-
-
-
-// const { MongoClient } = require("mongodb");
-
-// const uri = 'mongodb+srv://admin:tIaG5ePELrZa9QEd@cluster0.gpf3m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-// const options = {}
-
-// let client
-// let clientPromise
-
-
-
-
-// // if (process.env.NODE_ENV === 'development') {
-// //   // In development mode, use a global variable so that the value
-// //   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-// //   if (!global._mongoClientPromise) {
-// //     client = new MongoClient(uri, options)
-// //     global._mongoClientPromise = client.connect()
-// //   }
-// //   clientPromise = global._mongoClientPromise
-// // } else {
-// // In production mode, it's best to not use a global variable.
-// client = new MongoClient(uri, options)
-// clientPromise = client.connect()
-
-
-// // }
-
-// // TODO: 
-// //pagination or max records check
-// // validation content in AT fields 
-// //replace name of fields with IDs 
-
-// // const testTry = () => {
-//     apiKey = 'keyKcjxZCjui9Pqki'
-//     const Airtable = require("airtable");
-//     const AtBase = new Airtable({ apiKey: apiKey }).base('appXnj8G9efiiY9Ge');
-//     console.log('inside test try')
-
-//     let classesInfo = {}; //Cohorts cache
-//     AtBase('Classes').select({
-//     }).eachPage(function page(records, fetchNextPage) {
-//         records.forEach(function (record) {
-//             classesInfo[record.id] = record.fields;
-//         });
-//         fetchNextPage();
-//     }, function done(err) {
-//         if (err) { console.error(err); return; }
-//     });
-
-
-
-//     AtBase('Courses').select({
-//         view: "Default View",
-
-//     }).eachPage(function page(records, fetchNextPage) {
-//         records.forEach(function (record) {
-//             let course = {
-//                 course_name: record.get('Name'),
-//                 airtable_id: record.id,
-//             }
-//             // coursesToDb.push(course);
-//             insertData(course, 'Courses')
-//         });
-//         fetchNextPage();
-
-//     }, function done(err) {
-//         if (err) { console.error(err); return; }
-//     });
-
-
-
-//     let ATtoMongoCourseCache = {}
-
-//     const insertData = async (data, coll) => {
-//         // @param {<Object>} data 
-//         // @param {string} coll
-
-//         const client = await clientPromise;
-//         const db = client.db(process.env.MONGODB_DB);
-
-
-//         try {
-//             console.log("insert to Mongo", data)
-//             const result = db
-//                 .collection(coll)
-//                 .updateOne({}, { $set: data }, { upsert: true }); //course_name: data.course_name
-//             console.log('result = ', result)
-//             ATtoMongoCourseCache[item.airtable_id] = result._id
-//         }
-//         catch (error) {
-//             console.log(error, `Can't insert to db`);
-//         } finally {
-//             await client.close();
-//         }
-//     // };
-//     // testTry()
-//     console.log('after called test try')
-
-
-// }
