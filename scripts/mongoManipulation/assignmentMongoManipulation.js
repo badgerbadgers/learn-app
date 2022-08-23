ab8b47f65428ec66090d69bcb777cf93e4373873
dkprioleau <dkprioleau@gmail.com>
2022-08-23 17:19:12 -0400

    cleanup of assignment collection object

--- scripts/mongoManipulation/assignmentMongoManipulation.js
+++ scripts/mongoManipulation/assignmentMongoManipulation.js
@@ -1,23 +1,31 @@
 const utils = require("./utils.js");
-const { MongoClient } = require("mongodb"); 
+const { MongoClient } = require("mongodb");
 
-//this script is to clean up the assignment collection in mongo 
+//this script is to flatten the link object in the assignment collection 
 
-const minifyAssignments = async()=> {
-    const uri = await utils.getConfigParam(/^MONGODB_URI=(.+)/);
-    const client = new MongoClient(uri);
-    await client.connect();
-    const db = client.db("myFirstDatabase");
+const minifyAssignments = async () => {
+  const uri = await utils.getConfigParam(/^MONGODB_URI=(.+)/);
+  const client = new MongoClient(uri);
+  await client.connect();
+  const db = client.db("myFirstDatabase");
+  try {
 
-    try{  
-        const assignmentsArray = await db
-        .collection("assignments").aggregate([
-            
-        ])
+    await db.collection("assignments").updateMany(
+        // query
+        {link:{$exists:true}},
+        // update: [] for aggregation pipeline
+        [{$set:{link:"$link.url"}}], 
+        // options
+        {}
+    );
+  } catch (e) {
+    console.log("ERROR", e.message);
+  }
+};
 
-    }catch (e){ 
-        console.log("ERROR", e.message )
-    }
-} 
+minifyAssignments().then(() => {
+    console.log("all done");
+    process.exit(0);
+  });
 
-minifyAssignments()
+
