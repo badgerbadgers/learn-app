975ee53ee721e300c0fa5f0f422af68a9f9846ff
Omer <omeryariv@gmail.com>
2022-09-12 16:22:06 -0400

    restore some lines that were accidentaly deleted, formatting

--- scripts/airtableMigration/courseAirtableMongoMigration.js
+++ scripts/airtableMigration/courseAirtableMongoMigration.js
@@ -4,49 +4,57 @@ const upload = require("./insertToMongo");
 // This script is fetching courses and lessons from Airtable and inserting to mongo collection
 
 const getLessons = async () => {
-    const AtBase = await airtableConnection();
-    const lessonTitles = {};
-    await AtBase("Lessons").select({
-    }).eachPage(function page(records, fetchNextPage) {    
-        records.forEach((item) => {
-            lessonTitles[item.id] = item.get("Title")
-        });
-        fetchNextPage();
-    }).catch(err => {
-        console.error(err);
+  const AtBase = await airtableConnection();
+  const lessonTitles = {};
+  await AtBase("Lessons")
+    .select({})
+    .eachPage(function page(records, fetchNextPage) {
+      records.forEach((item) => {
+        lessonTitles[item.id] = item.get("Title");
+      });
+      fetchNextPage();
+    })
+    .catch((err) => {
+      console.error(err);
     });
-    return lessonTitles;
-}
-
+  return lessonTitles;
+};
 
 const getDataFromAirtable = async (lessonsCache) => {
-    const dataToMongo = []
-    const AtBase = await airtableConnection();
-    await AtBase("Courses").select({
-    }).eachPage(function page(records, fetchNextPage) {
-        console.log("Records #", records.length);
-        records.forEach(function (record) {
+  const dataToMongo = [];
+  const AtBase = await airtableConnection();
+  await AtBase("Courses")
+    .select({})
+    .eachPage(function page(records, fetchNextPage) {
+      console.log("Records #", records.length);
+      records.forEach(function (record) {
         //check for name to make sure we don't process empty rows
         if (record.get("Name")) {
-                    lessons: record.get("Lessons")?.map(lesson => lessonsCache[lesson]) || [],
-                }
-                console.log("Course goes to mongo", course);
-                dataToMongo.push(course);
-            }
-        });
-        fetchNextPage();
-    }, 
-
-    ).catch(err => { //instead of done(err)
-        console.error(err);
+          let course = {
+            course_name: record.get("Name"),
+            airtable_id: record.id,
+            lessons:
+              record.get("Lessons")?.map((lesson) => lessonsCache[lesson]) ||
+              [],
+          };
+          console.log("Course goes to mongo", course);
+          dataToMongo.push(course);
+        }
+      });
+      fetchNextPage();
+    })
+    .catch((err) => {
+      //instead of done(err)
+      console.error(err);
     });
-    return dataToMongo;
-}
+  return dataToMongo;
+};
 
-const main =async () => {
-    const lessons = await getLessons();
-    const toMongo = await getDataFromAirtable(lessons);
-    upload(toMongo, "courses"); // change the name of collection manually 
-}
+const main = async () => {
+  const lessons = await getLessons();
+  const toMongo = await getDataFromAirtable(lessons);
+  upload(toMongo, "courses"); // change the name of collection manually
+};
 
 main();
+
