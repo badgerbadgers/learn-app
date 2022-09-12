269bbf3a79be41f64dffd786234cdffa5377da72
Omer <omeryariv@gmail.com>
2022-09-12 15:53:59 -0400

    using loop instead of forEach so async is respected.

--- scripts/airtableMigration/insertToMongo.js
+++ scripts/airtableMigration/insertToMongo.js
@@ -20,19 +20,20 @@ const insertToMongo = async (data, coll) => {
   async function run() {
     try {
       const database = client.db(dbName);
-      const mongoCollectionName = database.collection(coll);
-      // data.forEach(doc => mongoCollectionName.insertOne(doc));
-      data.forEach((doc) => {
+      const collection = database.collection(coll);
+
+      for (doc of data) {
         const filter = {};
         //matching filtered collection and field to document collection
         filter[collectionFilterField[coll]] = doc[collectionFilterField[coll]];
-        const result = mongoCollectionName.updateOne(
+        // console.log(coll, filter, doc); continue;
+        const result = await collection.updateOne(
           filter,
           { $set: doc },
           { upsert: true }
         );
-        console.log(`This is the updated document: ${result} `);
-      });
+        console.log(`This is the updated document: ${JSON.stringify(result)} `);
+      }
     } finally {
       // Ensures that the client will close when finish/error
       // await client.close();
