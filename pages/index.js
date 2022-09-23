3a14a5a78a4c5fe508f45e360d3a596c0b9db404
Omer <omeryariv@gmail.com>
2022-09-23 10:48:28 -0400

    remove a bunch of unused css, html and imports

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,16 @@ import { getSession } from "next-auth/react";
 import LogIn from "../components/Login";
 import { Typography } from "@mui/material";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import ImageWall from "../components/ImageWall";
-import { publicLayout } from "../components/PublicLayout";
+import { publicLayout } from "../components/layout/PublicLayout";
 export default function Home({ user }) {
   return (
-    <>
-      {/* <ImageWall />  */}
-      <div className={styles.indexOverlay}></div>
-      <div className={styles.homeOuter}>
-        <div className={styles.left}></div>
-        <div className={styles.right}></div>
-        <div className={styles.inner}>
-          <Image alt="" src="/img/labs-g-01.png" width={315} height={240} />
-          <Typography variant="h5" sx={{ textAlign: "center" }}>
-            Building Together
-          </Typography>
-          <LogIn />
-        </div>
-      </div>
-    </>
+    <div>
+      <Image alt="" src="/img/labs-g-01.png" width={315} height={240} />
+      <Typography variant="h5" sx={{ textAlign: "center" }}>
+        Building Together
+      </Typography>
+      <LogIn />
+    </div>
   );
 }
 
@@ -50,3 +40,4 @@ export async function getServerSideProps(context) {
     };
   }
 }
+
