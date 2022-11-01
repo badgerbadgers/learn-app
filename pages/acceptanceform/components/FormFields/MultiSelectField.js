66b73026b6f937445cdda70ce862ce63924a0b24
Ignat Babenko <ignat@codethedream.org>
2022-11-01 09:17:10 -0700

    Work in progress

--- pages/acceptanceform/components/FormFields/MultiSelectField.js
+++ pages/acceptanceform/components/FormFields/MultiSelectField.js
@@ -8,7 +8,20 @@ import {
   Select,
   MenuItem,
   FormHelperText,
+  Checkbox,
+  ListItemText,
 } from "@mui/material";
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 function MultiSelectField(props) {
   const { label, data, ...rest } = props;
@@ -16,6 +29,18 @@ function MultiSelectField(props) {
   const { value: selectedValue } = field;
   const [touched, error] = at(meta, "touched", "error");
   const isError = touched && error && true;
+
+  const [personName, setPersonName] = React.useState([]);
+
+  // const handleChange = (event) => {
+  //   const {
+  //     target: { value },
+  //   } = event;
+  //   setPersonName(
+  //     // On autofill we get a stringified value.
+  //     typeof value === "string" ? value.split(",") : value
+  //   );
+  // };
 
   function renderHelperText() {
     if (isError) {
@@ -33,11 +58,17 @@ function MultiSelectField(props) {
         variant="outlined"
         fullWidth
         multiple
+        // onChange={handleChange}
+        // input={<OutlinedInput label="Tag" />}
+        // renderValue={(selected) => selected.join(", ")}
+        // MenuProps={MenuProps}
         // onChange={(e) => handleInputChange(e)}
         // value={userInfoData.department}
       >
         {data.map((item, index) => (
           <MenuItem key={index} value={item.value}>
+            {/* <Checkbox checked={personName.indexOf(item.value) > -1} />
+            <ListItemText primary={item.label} /> */}
             {item.label}
           </MenuItem>
         ))}
