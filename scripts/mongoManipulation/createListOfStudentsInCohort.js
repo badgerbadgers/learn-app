ab07981dfe9d8955be52602326bacbffe44956de
Olena Suvorova <olena@codethedream.org>
2022-12-12 21:13:04 -0800

    Refactor approach of updating students field.

--- scripts/mongoManipulation/createListOfStudentsInCohort.js
+++ scripts/mongoManipulation/createListOfStudentsInCohort.js
@@ -8,60 +8,53 @@ const { mongoConnection, getConfigParam } = require("../utils.js");
 
 
 const convertStudentsArr = async () => {
-    const confParam = await getConfigParam("MONGODB_DB");
-    const client = await mongoConnection();
-    const db = client.db(confParam);
+  const confParam = await getConfigParam("MONGODB_DB");
+  const client = await mongoConnection();
+  const db = client.db(confParam);
 
-    try {
-        const cohortsToUpdate = await db
-            .collection("cohorts")
-            .find(
-                // {students: {$gte: {$size: 1}}}, this query doesn't return the expected result
-                { 'students.0': { $exists: true } },
-            )
-            .toArray()
-        let bulkOperations = [];
-        cohortsToUpdate.map(c => {
-            let newStudents = [];
-            c.students.map(st => {
-                newStudents.push(
-                    {
-                        user_id: new ObjectId(st),
-                        added_at: new Date()
-                    }
-                )
-            })
-            bulkOperations.push(
-                {
-                    updateOne: {
-                        filter: {_id: c._id},
-                        update: { $set: { "students": newStudents } }
-                    }
+  try {
+    const results = await db
+      .collection("cohorts")
+      .aggregate([
+        {
+          $match: {
+            "students.0": {
+              $exists: true
+            }
+          }
+        }, {
+          $set: {
+            "students": {
+              $map: {
+                input: "$students",
+                in: {
+                  "user": "$$this",
+                  "added_at": "$$NOW"
                 }
-            )
-        })
-        await db.collection("cohorts")
-            .bulkWrite(bulkOperations)
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
+              }
+            }
+          }
+        },
+        {
+          $merge: {
+            on: "_id",
+            into: "cohorts",
+            whenMatched: "merge",
+            whenNotMatched: "discard",
+          },
+        }
+      ]).toArray();
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 };
 
 const run = async () => {
-    await convertStudentsArr();
-
-    console.log("all done");
-    process.exit(0);
+  await convertStudentsArr();
+  console.log("all done");
+  process.exit(0);
 };
 
 run();
-
-// $set: {
-//     "students.$[]": {
-//         "_id": { students.$[]._id },
-//         "added_at": new ObjectId(),
-//     }
-// }
