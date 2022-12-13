2da1bfb76c2004f4f93e7830a07e2f217cac9c52
badgerbadgers <tim.almacen@gmail.com>
2022-12-12 20:15:53 -0800

    props passed from getServerSideProps are from mongoDB and wordpress, created new function that combines both endpoints to create a new array of objects and added new key value pair isShown in StaticPages model

--- pages/admin/static-page-management/index.js
+++ pages/admin/static-page-management/index.js
@@ -3,57 +3,51 @@ import { Switch } from "@mui/material"
 import { DataGrid } from "@mui/x-data-grid"
 import NavBar from "../../../components/layout/NavBar"
 import Footer from "../../../components/layout/Footer"
+import StaticPage from "../../../lib/models/StaticPage"
 // import { PublicLayout2 } from "../../../components/layout/PublicLayout2"
+import dbConnect from "../../../lib/dbConnect"
 
-export default function StaticPage({ posts }) {
-  const [pages, setPages] = useState(posts)
-  const [checked, setChecked] = useState(true)
-  const [staticPages, setStaticPages] = useState([])
+export default function AllStaticPages({ wordpressData, mongoData, data }) {
+  let parsedMongoData = JSON.parse(mongoData)
+  const [wpPages, setwpPages] = useState(wordpressData)
+  let parsedData = JSON.parse(data)
+  const [checked, setChecked] = useState(parsedData.deleted_at)
+  const [mongoPages, setMongoPages] = useState(parsedMongoData)
 
-  console.log("useeffect", staticPages)
-  // useEffect(() => {
-  //   const url = "/api/staticpages"
-  //   try {
-  //     ;async () => {
-  //       await getData(url)
-  //         .then((res) => res.json())
-  //         .then((res) => setStaticPages(res.data))
-  //       // .then((res) => console.log(res))
-  //     }
-  //   } catch (error) {
-  //     console.log(error, "error")
-  //   }
-  // }, [])
-  useEffect(() => {
-    let newPage = {}
-    const url = "/api/staticpages"
-    //  + `${query.slug}`
-    // const params = { slug: query.slug }
-    try {
-      ;(async () => {
-        await fetch(url)
-          .then((res) => res.json())
-          .then((res) => setStaticPages(res.data))
-        // id = response.id
-        // deleted = response.deleted_at
-        // newPage = {
-        //   id,
-        //   deleted,
-        // }
-        // setStaticPages(newPage)
-        // setSchedule(cohort.schedule)
-        // setLoading(false)
-      })()
-    } catch (error) {
-      console.log("An error from getData in", url, error)
-    }
-  }, [])
+  // let parsedData = JSON.parse(data)
+  const [updatedPages, setUpdatedPages] = useState(parsedData)
+  // const [checked, setChecked] = useState(parsedData.deleted_at)
 
+  // console.log("data", parsedData)
+  // console.log("updated", updatedPages)
+
+  function newObj(data) {
+    // const newObj = (data) => {
+    let combinedObj = {}
+    let combinedArr = []
+    wordpressData.map((wpitem) => {
+      // console.log("wpitem", wpitem)
+      let item = parsedMongoData.find((i) => wpitem.id === i.wordpress_id)
+      if (item) {
+        combinedObj = {
+          id: wpitem.id,
+          title: item.title,
+          slug: item.slug,
+          deleted_at: item.deleted_at,
+        }
+        console.log("combined data object", combinedObj)
+        // return combinedObj
+      }
+      combinedArr.push(combinedObj)
+      console.log("combined mongo wordpress array", combinedArr)
+    })
+  }
+  // console.log("newobj", newObj)
+  newObj()
   const handleChange = async (event) => {
-    setChecked(!checked)
-    //type string
     let id = event.target.id
     let deleted = event.target.checked
+    console.log("deleted", deleted)
     await fetch("/api/staticpages", {
       method: "PATCH",
       // body: id,
@@ -63,31 +57,61 @@ export default function StaticPage({ posts }) {
         "Content-Type": "application/json",
       },
     })
-      // await fetch("/api/staticpages", { method: "PATCH" })
       .then((res) => res.json())
-      // .then((res) => console.log(res))
-      .then((res) => setStaticPages(res.data))
+      .then((res) => setUpdatedPages(res.data))
   }
-  const columns = [
+
+  // const columns = [
+  //   {
+  //     // field: "slug",
+  //     field: "title",
+  //     headerName: "Title",
+  //     width: 450,
+  //     valueGetter: (params) => {
+  //       return params.row.title.rendered
+  //     },
+  //   },
+  //   { field: "id", headerName: "ID", width: 150 },
+  //   {
+  //     field: "col3",
+  //     headerName: "Shown in Learn App",
+  //     width: 150,
+  //     renderCell: (params) => {
+  //       const id = params.id
+  //       // const deleted = params.deleted_at
+  //       return (
+  //         <Switch
+  //           id={id.toString()}
+  //           checked={checked}
+  //           // checked={deleted}
+  //           onChange={handleChange}
+  //           inputProps={{ "aria-label": "controlled" }}
+  //         />
+  //       )
+  //     },
+  //   },
+  // ]
+
+  const columns2 = [
     {
       // field: "slug",
       field: "title",
       headerName: "Title",
       width: 450,
       valueGetter: (params) => {
-        return params.row.title.rendered
+        return params.row.title
       },
     },
-    { field: "id", headerName: "ID", width: 150 },
+    { field: "wordpress_id", headerName: "ID", width: 150 },
     {
-      field: "col3",
+      field: "shown",
       headerName: "Shown in Learn App",
       width: 150,
       renderCell: (params) => {
-        const id = params.id
+        // const id = params.id
         return (
           <Switch
-            id={id.toString()}
+            // id={id.toString()}
             checked={checked}
             onChange={handleChange}
             inputProps={{ "aria-label": "controlled" }}
@@ -102,7 +126,14 @@ export default function StaticPage({ posts }) {
       <NavBar />
       <div style={{ height: 800, width: "100%" }}>
         <h2>WordPress pages</h2>
-        <DataGrid columns={columns} rows={pages} />
+        {/* <DataGrid columns={columns} rows={wpPages} /> */}
+        <DataGrid
+          // {...parsedMongoData}
+          //           {...parsedMongoData, checked: parsedMongoData.deleted_at}
+          columns={columns2}
+          rows={updatedPages}
+          getRowId={(row) => row.wordpress_id}
+        />
       </div>
       <Footer />
     </>
@@ -110,16 +141,53 @@ export default function StaticPage({ posts }) {
 }
 
 export async function getServerSideProps() {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
+  await dbConnect()
+  const mongoData = await StaticPage.find({}).lean()
   const res = await fetch("https://learn.codethedream.org/wp-json/wp/v2/pages")
-  const posts = await res.json()
+  const wordpressData = await res.json()
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
+  /*
+  [x]new func combine data
+  map if isshown keep mongo _id
+  [x]take needed properties from wp data
+  [x] const newObj =  {}
+  */
+
+  // let combinedObj = {}
+  // let combinedData = []
+  function combinedData(data) {
+    wordpressData.map((wpitem) => {
+      // console.log("wpitem", wpitem)
+      let combinedObj = {}
+      let combinedData = []
+      let item = mongoData.find(
+        (mongoitem) => wpitem.id === mongoitem.wordpress_id
+      )
+      if (item) {
+        combinedObj = {
+          wordpress_id: wpitem.id,
+          title: item.title,
+          slug: item.slug,
+          deleted_at: item.deleted_at,
+        }
+        // console.log("combo", combinedObj)
+        // return combinedObj
+        // emptyArr.push(combinedObj)
+      }
+      combinedData.push(combinedObj)
+      console.log("ssprops combined data", combinedData)
+    })
+  }
+  combinedData()
+
+  console.log("combined data after function call", combinedData)
+
   return {
     props: {
-      posts,
+      wordpressData,
+      mongoData: JSON.stringify(mongoData),
+      data: JSON.stringify(mongoData),
+      // combinedData,
     },
   }
 }
