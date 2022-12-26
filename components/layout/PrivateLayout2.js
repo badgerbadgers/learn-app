c07784c735e4dd4c7ea14e16754a9ecf2b286a98
badgerbadgers <tim.almacen@gmail.com>
2022-12-26 12:33:04 -0800

    deleted PrivateLayout2.js, Footer2.js, Footer2.module.css and comment footer height in gobals.css line 86, created new class name to target Footer component and style that component for better placement underneat datagrid, static-page-management/index.js file has been updated with imported file names

--- components/layout/PrivateLayout2.js
+++
@@ -1,7 +0,0 @@
-export const PrivateLayout2 = ({ children }) => (
-  <>
-    <main className='display'>{children}</main>
-  </>
-)
-
-export const privateLayout2 = (page) => <PrivateLayout2>{page}</PrivateLayout2>
