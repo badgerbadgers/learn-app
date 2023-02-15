ca6cae97d890652761e6fb9de9bde2e004de31e8
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2023-02-15 13:22:16 -0600

    deleted todo comments

--- migrations/20230208183047-change-isAdmin-to-is_admin.js
+++ migrations/20230208183047-change-isAdmin-to-is_admin.js
@@ -1,7 +1,5 @@
 module.exports = {
   async up(db, client) {
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
     try {
       await db.collection('users').updateMany({}, {$rename:{"isAdmin":"is_admin"}}, false, true)
     } catch (error) {
@@ -10,7 +8,6 @@ module.exports = {
   },
 
   async down(db, client) {
-    // TODO write the statements to rollback your migration (if possible)
     try {
       await db.collection('users').updateMany({}, {$rename:{"is_admin":"isAdmin"}}, false, true)
     } catch (error) {
