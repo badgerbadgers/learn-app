e000e40569433e4384aaabe595064df25c846d54
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2023-02-15 13:23:45 -0600

    deleted todo comments

--- migrations/20230202175723-remove-hasProfile-field-from-user.js
+++ migrations/20230202175723-remove-hasProfile-field-from-user.js
@@ -1,12 +1,9 @@
 module.exports = {
   async up(db, client) {
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
     await db.collection('users').updateMany({}, {$unset: {hasProfile: ""}});
   },
 
   async down(db, client) {
-    // TODO write the statements to rollback your migration (if possible)
     await db.collection('users').updateMany({}, {$set: {hasProfile: true}});
   }
 };
