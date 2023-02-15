4e6789b1aa0725cb25f0de4e9568b25b22638e48
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2023-02-15 13:23:22 -0600

    deleted todo comments

--- migrations/20230203175104-remove-roles-collection.js
+++ migrations/20230203175104-remove-roles-collection.js
@@ -1,15 +1,12 @@
 module.exports = {
   async up(db, client) {
-    // TODO write your migration here.
-    // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
     await db.collection('users').updateMany({}, {$unset: {roleIds: ""}});
     await db.collection('roles').drop();
 
   },
 
   async down(db, client) {
-    // TODO write the statements to rollback your migration (if possible)
-    // Example:
+   
     await db.collection('roles').insertOne({
       id: "1",
       name: "Admin"
