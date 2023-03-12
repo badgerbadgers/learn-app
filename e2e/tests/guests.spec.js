edb5ae5bcbaa58ed36e01cb3cbac944b94453945
Omer <omeryariv@gmail.com>
2023-03-12 16:28:00 -0400

    example of testing for both guest and user

--- e2e/tests/guests.spec.js
+++ e2e/tests/guests.spec.js
@@ -1,23 +1,46 @@
 import { test, expect } from "@playwright/test";
 
-test("Guests are asked to login when loading root", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
 
-  const logInButton = page.getByRole("button", { name: /log-in/i });
-  await expect(logInButton).toBeVisible();
+test.describe("Guest Users", () => {
+  test("are asked to login when loading root", async ({ page }) => {
+    //get browser console log and display them
+    // page.on("console", (msg) => console.log(msg.text()));
 
-  const signUpButton = page.getByRole("button", { name: /sign-up/i });
-  await expect(signUpButton).toBeVisible();
+    await page.goto("http://localhost:3000/dashboard");
+
+    const logInButton = page.getByRole("button", { name: /log-in/i });
+    await expect(logInButton).toBeVisible();
+
+    const signUpButton = page.getByRole("button", { name: /sign-up/i });
+    await expect(signUpButton).toBeVisible();
+  });
 });
 
-test("Guests are asked to login when loading another protected page", async ({
-  page,
-}) => {
-  await page.goto("http://localhost:3000/admin/cohort-management");
+test.describe("Logged In Users", () => {
+  console.log(process.env);
+  test.use({ storageState: process.env.STORAGE_STATE_USER });
 
-  const logInButton = page.getByRole("button", { name: /log-in/i });
-  await expect(logInButton).toBeVisible();
+  test("Can view the dashboard", async ({ page }) => {
+    //get browser console log and display them
+    // page.on("console", (msg) => console.log(msg.text()));
 
-  const signUpButton = page.getByRole("button", { name: /sign-up/i });
-  await expect(signUpButton).toBeVisible();
+    await page.goto("http://localhost:3000/dashboard");
+
+    const logInButton = page.getByRole("button", { name: /log-in/i });
+    await expect(logInButton).toHaveCount(0);
+  });
 });
+
+
+
+// test("Guests are asked to login when loading another protected page", async ({
+//   page,
+// }) => {
+//   await page.goto("http://localhost:3000/admin/cohort-management");
+
+//   const logInButton = page.getByRole("button", { name: /log-in/i });
+//   await expect(logInButton).toBeVisible();
+
+//   const signUpButton = page.getByRole("button", { name: /sign-up/i });
+//   await expect(signUpButton).toBeVisible();
+// });
