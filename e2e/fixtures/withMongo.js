7ef5fa16210932f906ff585bbf70a2f4f39d5374
Omer <omeryariv@gmail.com>
2023-03-14 14:30:18 -0400

    new fixture with access to mongo and data reset

---
+++ e2e/fixtures/withMongo.js
@@ -0,0 +1,46 @@
+import { test as base, Page, Browser } from "@playwright/test";
+import { MongoClient, MongoNotConnectedError } from "mongodb";
+
+const resetData = async function () {
+  //grab userids for automatically created users
+  const usersToKeep = await this.collection("users").distinct("_id", {
+    email: { $in: ["admin@codethedream.org", "user@codethedream.org"] },
+  });
+
+  //reset users to only have auto created users
+  await this.collection("users").deleteMany({ _id: { $nin: usersToKeep } });
+
+  //reset sessions and accounts to only have data for auto created users
+  await this.collection("sessions").deleteMany({
+    userId: { $nin: usersToKeep },
+  });
+  await this.collection("accounts").deleteMany({
+    userId: { $nin: usersToKeep },
+  });
+
+  //reset all other collections
+  const collsToSkip = ["users", "accounts", "sessions"];
+  const collections = (await this.listCollections().toArray()).map(
+    (c) => c.name
+  );
+  //for each collection
+  for (const c of collections) {
+    //if the collection name is not users, accounts or sessions
+    if (collsToSkip.indexOf(c) === -1) {
+      //drop it
+      this.collection(c).drop();
+    }
+  }
+};
+
+export * from "@playwright/test";
+export const test = base.extend({
+  db: async ({}, use) => {
+    const mongo = new MongoClient(process.env.MONGODB_URI);
+    await mongo.connect();
+    const db = mongo.db(process.env.MONGODB_DB);
+    db.resetData = resetData;
+    await use(db);
+    await db.resetData();
+  },
+});
