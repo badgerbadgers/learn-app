b975648c17ef2d40e604c61ef6d99a943e4612f7
Omer <omeryariv@gmail.com>
2023-03-14 14:11:24 -0400

    removed comments

--- e2e/tests/api/example/getUsers.spec.js
+++ e2e/tests/api/example/getUsers.spec.js
@@ -1,17 +1,10 @@
 import { test, expect } from "e2e/fixtures/testAsAdmin";
 
 test.describe("example of testing API", () => {
-  test("should get the list of users", async ({ request }) => {
+  test("should get the list of users", async ({ request, db }) => {
     const response = await request.get(`/api/users`);
     expect(response).toBeOK();
     const users = (await response.json()).data;
     expect(users).toHaveLength(2);
-
-    // const issues = await request.get(`/repos/${USER}/${REPO}/issues`);
-    // expect(issues.ok()).toBeTruthy();
-    // expect(await issues.json()).toContainEqual(expect.objectContaining({
-    //   title: '[Bug] report 1',
-    //   body: 'Bug description'
-    // }));
   });
 });
