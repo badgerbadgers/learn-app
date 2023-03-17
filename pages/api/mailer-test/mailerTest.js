bd430db4168d077a9db94454092db0ae4c0c7b9b
OxiBo <xenos2000@ukr.net>
2023-03-17 15:55:38 -0400

    Minor code cleanup

--- pages/api/mailer-test/mailerTest.js
+++ pages/api/mailer-test/mailerTest.js
@@ -1,4 +1,4 @@
-// this route is here only temporarily for manual testing purposes 
+// TODO - DELETE THE ROUTE; this route is here only temporarily for manual testing purposes
 import mailer from '../../../lib/mailer/mailer';
 
 export default async function handler(req, res) {
@@ -11,16 +11,15 @@ export default async function handler(req, res) {
       res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
-// 
+//
 const sendMail = async (req, res) => {
-
-    // mailer function takes the following parameters:  subject, content, sendTo, copyTo (optional), text (optional, set to undefined by default in the function expression, text cannot be set to an empty string) 
+  // mailer function takes the following parameters:  subject, content, sendTo, copyTo (optional), text (optional, text cannot be set to an empty string)
   try {
     await mailer(
       'test email',
       '<div>some content</div>',
-      '********@codethedream.org',
-    ); // use the email you want to receive the email at 
+      'your-email@****.****'
+    ); // use the email you want to receive the email at
 
     res.status(200).json({ success: true });
   } catch (error) {
