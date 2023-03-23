defac148a23e6f5f5c1d19f92c53d1f77df70162
Omer <omeryariv@gmail.com>
2023-03-23 12:06:16 -0400

    add loadData method for loading seed data into db

--- e2e/fixtures/withMongo.js
+++ e2e/fixtures/withMongo.js
@@ -1,5 +1,7 @@
 import { test as base, Page, Browser } from "@playwright/test";
 import { MongoClient, MongoNotConnectedError } from "mongodb";
+import { Seeder } from "mongo-seeding";
+import path from "node:path";
 
 const resetData = async function () {
   //grab userids for automatically created users
@@ -33,13 +35,38 @@ const resetData = async function () {
   }
 };
 
+const loadData = async function (pathToData) {
+  const collections = await this.seeder.readCollectionsFromPath(
+    path.resolve(pathToData)
+    // collectionReadingOptions,
+  );
+  try {
+    await this.seeder.import(collections);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 export * from "@playwright/test";
 export const test = base.extend({
   db: async ({}, use) => {
+    //prepare a mongo client using the test db
     const mongo = new MongoClient(process.env.MONGODB_URI);
     await mongo.connect();
     const db = mongo.db(process.env.MONGODB_DB);
+
+    //add custom method for returning data to minimal
     db.resetData = resetData;
+
+    //prepare seeder for easily loading data into mongo
+    //(see https://github.com/pkosiec/mongo-seeding)
+    const seederConfig = {
+      database: process.env.MONGODB_URI,
+      dropDatabase: false, //we don't want to drop the db, as we want to keep our basic user data
+    };
+    db.seeder = new Seeder(seederConfig);
+    db.loadData = loadData;
+
     await use(db);
     //TODO: should we reset data between every test?
     //It makes sense on the surface, but can get dicey with running multiple tests in parallel.
