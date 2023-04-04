0f9e81e0c4b536ea6a0480da4dcf0ad0005e2451
OxiBo <xenos2000@ukr.net>
2023-04-04 10:13:57 -0400

    Add props to Swagger ui to disable 'try it out' button for certain request methods

--- pages/dev/api-docs.js
+++ pages/dev/api-docs.js
@@ -1,20 +1,23 @@
-import { createSwaggerSpec } from "next-swagger-doc";
-import dynamic from "next/dynamic";
-import "swagger-ui-react/swagger-ui.css";
+import { createSwaggerSpec } from 'next-swagger-doc';
+import dynamic from 'next/dynamic';
+import 'swagger-ui-react/swagger-ui.css';
 
-const SwaggerUI = dynamic(import("swagger-ui-react"), { ssr: false });
+const SwaggerUI = dynamic(import('swagger-ui-react'), { ssr: false });
 
-function ApiDoc({ spec }) {
-  return <SwaggerUI spec={spec} />;
+// adding a prop supportedSubmitMethods disables 'try it' button for not listed methods, 'try it out' button won't be shown
+function ApiDoc({ spec, supportedSubmitMethods }) {
+  return (
+    <SwaggerUI spec={spec} supportedSubmitMethods={supportedSubmitMethods} />
+  );
 }
 
 export const getStaticProps = async () => {
   const spec = createSwaggerSpec({
     definition: {
-      openapi: "3.0.0",
+      openapi: '3.0.0',
       info: {
-        title: "Next Swagger API ",
-        version: "1.0",
+        title: 'Next Swagger API ',
+        version: '1.0',
       },
     },
   });
@@ -22,6 +25,7 @@ export const getStaticProps = async () => {
   return {
     props: {
       spec,
+      supportedSubmitMethods: ['get'],
     },
   };
 };
--- pages/dev/api-docs/v1.js
+++ pages/dev/api-docs/v1.js
@@ -1,28 +1,31 @@
-import { createSwaggerSpec } from "next-swagger-doc";
-import dynamic from "next/dynamic";
-import "swagger-ui-react/swagger-ui.css";
+import { createSwaggerSpec } from 'next-swagger-doc';
+import dynamic from 'next/dynamic';
+import 'swagger-ui-react/swagger-ui.css';
 
-const SwaggerUI = dynamic(import("swagger-ui-react"), { ssr: false });
+const SwaggerUI = dynamic(import('swagger-ui-react'), { ssr: false });
 
-function ApiDoc({ spec }) {
-  return <SwaggerUI spec={spec} />;
+// adding a prop supportedSubmitMethods disables 'try it' button for not listed methods, 'try it out' button won't be shown
+function ApiDoc({ spec, supportedSubmitMethods }) {
+  return (
+    <SwaggerUI spec={spec} supportedSubmitMethods={supportedSubmitMethods} />
+  );
 }
-
 export const getStaticProps = async () => {
   const spec = createSwaggerSpec({
     definition: {
-      openapi: "3.0.0",
+      openapi: '3.0.0',
       info: {
-        title: "Learn App API",
-        version: "1.0",
+        title: 'Learn App API',
+        version: '1.0',
       },
     },
-    apiFolder: "pages/api/v1",
+    apiFolder: 'pages/api/v1',
   });
 
   return {
     props: {
       spec,
+      supportedSubmitMethods: ['get'],
     },
   };
 };
