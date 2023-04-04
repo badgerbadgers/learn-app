488e0c880c3dc7eb38cabba597abd151b6402f2e
badgerbadgers <tim.almacen@gmail.com>
2023-04-03 18:03:26 -0700

    static page variable in end2end test is not hard coded value passed in to mongoose method

--- e2e/tests/api/v1/staticpages/slug/index.spec.js
+++ e2e/tests/api/v1/staticpages/slug/index.spec.js
@@ -2,12 +2,13 @@ import { test, expect } from "e2e/fixtures/testAsAdmin";
 
 test.describe("/api/v1/staticpages/[slug]", () => {
   //GET SLUG TESTS
-  test("gets a static page using slug", async ({ request, db }) => {
-    const testslug = "bass-practicum";
+  test("gets a static page using slug as argument", async ({ request, db }) => {
+    //gets a document with slug value that is not null
     const staticpageslug = await db.collection("staticpages").findOne({
-      slug: testslug,
+      slug: { $ne: null },
     });
 
+    //hit endpoint with slug as path parameter
     const response = await request.get(
       `/api/v1/staticpages/${staticpageslug.slug}`
     );
