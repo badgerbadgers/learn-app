93097e10ae76e5d275a96c1cbe52e0a2d4101c5a
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-04-05 10:37:13 -0400

    GET cohort's schedule tests

---
+++ e2e/tests/api/v1/cohorts/id/schedule.spec.js
@@ -0,0 +1,74 @@
+import { test, expect } from "e2e/fixtures/testAsAdmin";
+
+test.describe("/api/v1/cohorts/[id]/schedule", () => {
+  //GET TESTS
+  test("returns schedule array of a non deleted cohort by cohort id", async ({
+    request,
+    db,
+  }) => {
+    //find not deleted cohort that has NON empty schedule array
+    //at this moment only Intro cohorts has schedule array
+    const nonEmptyScheduleCohort = await db.collection("cohorts").findOne({
+      $and: [
+        { deleted_at: { $eq: null } },
+        { schedule: { $ne: [] } },
+        { schedule: { $ne: null } },
+      ],
+    });
+
+    //call GET and get the non-deleted cohort by id
+    const response = await request.get(
+      `/api/v1/cohorts/${nonEmptyScheduleCohort._id}/schedule`
+    );
+
+    const data = (await response.json()).data.schedule;
+
+    // check if response is OK
+    expect(response.ok()).toBeTruthy();
+    // check if returned data is an array
+    expect(Array.isArray(data)).toBe(true);
+    // check if array of schedule has expected length
+    expect(data).toHaveLength(nonEmptyScheduleCohort.schedule.length);
+
+    // check if each element of the array has a property 'type' and 'section'
+    data.forEach((schedule) => {
+      expect(schedule).toHaveProperty("type");
+      expect(schedule).toHaveProperty("section");
+    });
+    // Check if the schedule array has an object with property 'lesson' or 'content'
+    const hasLessonOrContent = data.some(
+      (item) => item.hasOwnProperty("lesson") || item.hasOwnProperty("content")
+    );
+    expect(hasLessonOrContent).toBe(true);
+    //find non deleted cohort that has  EMPTY schedule array
+    const emptyScheduleCohort = await db
+      .collection("cohorts")
+      .findOne({ deleted_at: { $eq: null }, schedule: { $eq: [] } });
+
+    const responseEmptySchedule = await request.get(
+      `/api/v1/cohorts/${emptyScheduleCohort._id}/schedule`
+    );
+    const emptyScheduleData = (await responseEmptySchedule.json()).data
+      .schedule;
+
+    // check if response is OK
+    expect(responseEmptySchedule.ok()).toBeTruthy();
+    // check an empty array is returned
+    expect(emptyScheduleData).toHaveLength(0);
+  });
+  ////////////////////////////////////////////////////////////////////
+  test("does not return schedule of a deleted cohort", async ({
+    request,
+    db,
+  }) => {
+    const randomDeletedCohort = await db.collection("cohorts").findOne({
+      deleted_at: { $ne: null },
+    });
+    const responseDeletedCohort = await request.get(
+      `/api/v1/cohorts/${randomDeletedCohort._id}/schedule`
+    );
+    // test if api does not return ok response if cohort is deleted
+    expect(responseDeletedCohort.ok()).not.toBeTruthy();
+    expect(responseDeletedCohort.status()).toBe(400);
+  });
+});
