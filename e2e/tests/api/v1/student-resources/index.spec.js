d0290f3dd79d65a3256b90b05a332912cbac4afe
badgerbadgers <tim.almacen@gmail.com>
2023-04-11 16:37:11 -0700

    cleaned up index.spec.js test file for student-resources endpoint

--- e2e/tests/api/v1/student-resources/index.spec.js
+++ e2e/tests/api/v1/student-resources/index.spec.js
@@ -1,13 +1,6 @@
 import { test, expect } from "e2e/fixtures/testAsAdmin";
-import { faker } from "@faker-js/faker";
 
 test.describe("/api/v1/staticpages", () => {
-  //tests for GET req
-  // [x] There is data to return
-  //dont implement error tests for now JEST will run these tests and Playwright cannot
-  //tests for POST req
-  // [x] creates new static page with isShown, wordpress_id, slug and title
-
   //GET TESTS
   test.only("returns an array when there are staticpages", async ({
     request,
@@ -17,12 +10,12 @@ test.describe("/api/v1/staticpages", () => {
     expect(res.ok()).toBeTruthy();
 
     const staticpages = (await res.json()).data;
-    console.log("statics", staticpages);
+
     //check for object containing isShown = true exists
     expect(staticpages).toContainEqual(
       expect.objectContaining({ isShown: true })
     );
-    //check for object containing isShown = false exists
+    //check for object containing isShown = false does not exists
     expect(staticpages).not.toContainEqual(
       expect.objectContaining({ isShown: false })
     );
