63b48ffc89d00efd11c392e57aafccc9b0a8ea1c
OxiBo <xenos2000@ukr.net>
2023-04-12 15:04:15 -0400

    Fix formatting issue: switch from double quotes to single ones

--- pages/dev/api-docs.js
+++ pages/dev/api-docs.js
@@ -1,8 +1,8 @@
-import { createSwaggerSpec } from 'next-swagger-doc';
-import dynamic from 'next/dynamic';
-import 'swagger-ui-react/swagger-ui.css';
+import { createSwaggerSpec } from "next-swagger-doc";
+import dynamic from "next/dynamic";
+import "swagger-ui-react/swagger-ui.css";
 
-const SwaggerUI = dynamic(import('swagger-ui-react'), { ssr: false });
+const SwaggerUI = dynamic(import("swagger-ui-react"), { ssr: false });
 
 // adding a prop supportedSubmitMethods disables 'try it' button for not listed methods, 'try it out' button won't be shown
 function ApiDoc({ spec, supportedSubmitMethods }) {
@@ -14,10 +14,10 @@ function ApiDoc({ spec, supportedSubmitMethods }) {
 export const getStaticProps = async () => {
   const spec = createSwaggerSpec({
     definition: {
-      openapi: '3.0.0',
+      openapi: "3.0.0",
       info: {
-        title: 'Next Swagger API ',
-        version: '1.0',
+        title: "Next Swagger API ",
+        version: "1.0",
       },
     },
   });
@@ -25,7 +25,7 @@ export const getStaticProps = async () => {
   return {
     props: {
       spec,
-      supportedSubmitMethods: ['get'],
+      supportedSubmitMethods: ["get"],
     },
   };
 };
