a16a8e45983fba35ca459ab4157c253b3cd9e28a
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-04-13 12:28:39 -0400

    added toHaveProperty check

--- e2e/tests/api/v1/lessons/index.spec.js
+++ e2e/tests/api/v1/lessons/index.spec.js
@@ -7,5 +7,19 @@ test.describe("/api/v1/lessons", () => {
     //call GET and get all the  lessons
     const response = await request.get(`/api/v1/lessons`);
     expect(response.ok()).toBeTruthy();
+    const lessons = (await response.json()).data;
+
+    // Check that the response is an array
+    expect(Array.isArray(lessons)).toBeTruthy();
+
+    // Check that each lesson object has the expected properties
+    lessons.forEach((lesson) => {
+      expect(lesson).toHaveProperty("_id");
+      expect(lesson).toHaveProperty("lesson_label");
+      expect(lesson).toHaveProperty("submission_link");
+      expect(lesson).toHaveProperty("title");
+      expect(lesson).toHaveProperty("section");
+    });
   });
+
 });
