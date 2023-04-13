64937b15b756e290ad467a909ed93f907b550eec
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-04-13 10:48:08 -0400

    GET lessons api endpoint

---
+++ pages/api/v1/lessons/index.js
@@ -0,0 +1,63 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Lessons
+ * /api/v1/lessons:
+ *   get:
+ *     description: Returns all lessons
+ *     tags: [Lessons]
+ *     parameters:
+ *        - in: query
+ *          name: cohort
+ *          type: string
+ *          required: false
+ *        - in: query
+ *          name: course
+ *          type: string
+ *          required: false
+ *     responses:
+ *       200:
+ *         description: Provides an array of lessons
+ *       400:
+ *         description: Error messages
+ */
+
+import Lesson from "lib/models/Lesson";
+import dbConnect from "lib/dbConnect";
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        //call method for getting lessons with any parameters we received
+        const lessons = await getLessons(req.query);
+        res.status(200).json({ data: lessons });
+      } catch (error) {
+        res.status(400).json({ message: error.message });
+      }
+      return;
+    case "POST":
+      try {
+      } catch (error) {}
+      return;
+    default:
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
+
+export const getLessons = async () => {
+  try {
+    await dbConnect();
+
+    const lessons = await Lesson.find();
+    if (!lessons) {
+      throw new Error("No lessons found");
+    }
+    return lessons;
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
