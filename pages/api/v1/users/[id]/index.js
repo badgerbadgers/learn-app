40e88946605e1836982d4f1651063e2304bbfad9
Oxana Ermolenko <oka@Oxanas-Mac-mini.local>
2023-04-17 00:00:44 -0700

    Use update variable inside PATCH case

--- pages/api/v1/users/[id]/index.js
+++ pages/api/v1/users/[id]/index.js
@@ -29,7 +29,6 @@ import dbConnect from "lib/dbConnect";
 export default async function handler(req, res) {
   const { method } = req;
   const { id } = req.query;
-  const updates = req.body;
   switch (method) {
     case "GET":
       try {
@@ -44,7 +43,7 @@ export default async function handler(req, res) {
     case "PATCH":
       try {
         //call method for updating user by id filds: name, email, gh
-        const user = await updateUser(id, updates);
+        const user = await updateUser(id, req.body);
         res.status(200).json({ data: user });
       } catch (error) {
         res.status(400).json({
