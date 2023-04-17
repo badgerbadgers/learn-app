529f39eeda43a9b58eb19208413e7f3871dfe47b
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2023-04-17 14:39:10 -0400

    Added zoom_link and seats to the swagger / moved the main logic into a separate function

--- pages/api/v1/cohorts/[id]/index.js
+++ pages/api/v1/cohorts/[id]/index.js
@@ -26,6 +26,12 @@
 *               type: string
 *               format: date-time
 *               example: 2025-04-04T04:00:00.000+00:00
+*             zoom_link:
+*               type: string
+*               example: https://us02web.zoom.us/j/87054407958
+*             seats:
+*               type: number
+*               example: 100
  *     responses:
  *       200:
  *         description: Updated cohort fields
@@ -38,44 +44,62 @@ import dbConnect from "lib/dbConnect";
 export default async function handler(req, res) {
   const { method } = req;
   const id = req.query.id;
+  const updates = req.body;
   await dbConnect();
   switch (method) {
     case "PATCH":
-      //check body for update field
-      const updates = req.body;
-
-      //first remove any keys that are not allowed to be updated
-      const allowedFields = ["cohort_name", "start_date", "seats", "zoom_link"];
-      for (const key in updates) {
-        if (allowedFields.indexOf(key) === -1) {
-          // delete updates[key];
-          return res.status(400).json({
-            data: updates,
-            message: `Update is Not Allowed`,
-          });
-        }
+      try {
+        await updateCohort(id, updates);
+        res.status(200).json({ data: updates });
+      } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: error.message });
       }
-      //Prevent changing cohort_name if it already exists
-      const cohortNameExists = await Cohort.findOne({
-        cohort_name: updates.cohort_name,
-      });
-
-      if (cohortNameExists) {
-        return res.status(400).json({ message: "Cohort name already exists" });
-      }
-      if (Object.keys(updates).length > 0) {
-        //update
-        try {
-          await Cohort.updateOne({ _id: id }, updates);
-        } catch (error) {
-          res.status(400).json({ success: false, message: error.message });
-          return;
-        }
-      }
-      res.status(200).json({ success: true, data: updates });
-      return;
+      break;
     default:
       res.setHeader("Allow", ["PATCH"]);
       res.status(405).end(`Method ${method} Not Allowed`);
+  }
+  return res;
+}
+
+export const updateCohort = async (id, updates) => {
+  //first remove any keys that are not allowed to be updated
+  const allowedFields = ["cohort_name", "start_date", "seats", "zoom_link"];
+
+  for (const key in updates) {
+    if (allowedFields.indexOf(key) === -1) {
+      throw new Error("Update is Not Allowed");
+    }
+  }
+  //Prevent changing cohort_name if it already exists
+  const cohortNameExists = await Cohort.findOne({
+    cohort_name: updates.cohort_name,
+  });
+
+  if (cohortNameExists) {
+    throw new Error("Cohort name already exists");
+  }
+  if (Object.keys(updates).length === 0) {
+    throw new Error("No valid information was supplied to update the cohort");
+  } else {
+    const updatedCohort = await Cohort.findByIdAndUpdate(id, updates, {
+      new: true,
+    });
+    //run mongoose validator to make sure data is valid
+    const validationErr = await updatedCohort.validate();
+    if (validationErr) {
+      throw new Error(validationErr);
+    }
+
+    if (!updatedCohort) {
+      const error = new Error();
+      error.status = 404;
+      error.message = `Could not find and update cohort with id - ${id}`;
+      throw error;
+    }
+    const updatesToDB = await Cohort.updateOne({ _id: id }, updates);
+
+    return updatesToDB;
   }
 }
