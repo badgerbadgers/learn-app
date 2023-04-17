ace304d452e19d233165cde58eb52c38a6e9ff29
badgerbadgers <tim.almacen@gmail.com>
2023-04-17 08:57:57 -0700

    added more tests for staticpages/id/index.spec.js for PATCH request and soft DELETE request

--- e2e/tests/api/v1/staticpages/id/index.spec.js
+++ e2e/tests/api/v1/staticpages/id/index.spec.js
@@ -3,10 +3,6 @@ import { faker } from "@faker-js/faker";
 
 const { ObjectId } = require("mongodb");
 test.describe("/api/v1/staticpages/[id]", () => {
-  //test
-  //A test for updating just one or two fields []
-  //A test for trying to update a field that doesn't exist []
-  //A test for undeleting a static page []
   //PATCH TESTS
   test("returns an updated static page that contains at least one or more updated field(s)", async ({
     request,
@@ -17,14 +13,27 @@ test.describe("/api/v1/staticpages/[id]", () => {
       .collection("staticpages")
       .findOne({ wordpress_id: { $ne: null } });
 
-    //PATCH OBJ
+    //FULL PATCH OBJ
     const allFieldsUpdated = {
       isShown: faker.datatype.boolean(),
       wordpress_id: faker.datatype.number(10000),
       slug: faker.lorem.slug(2),
       title: faker.lorem.words(2),
     };
-    //PATCH REQ
+
+    //PARTIAL PATCH OBJ
+    const twoFieldsUpdated = {
+      isShown: faker.datatype.boolean(),
+      wordpress_id: faker.datatype.number(10000),
+    };
+
+    //NON EXISTANT OBJ FIELDS
+    const nonExistantFields = {
+      fakeField: "this wont work",
+      fakeField2: "this also wont work",
+    };
+
+    //PATCH REQ FOR ALL FIELDS
     const updatedresponse = await request.patch(
       `/api/v1/staticpages/${staticPageToUpdate._id}`,
       {
@@ -32,38 +41,71 @@ test.describe("/api/v1/staticpages/[id]", () => {
       }
     );
 
+    //PATCH RESPONSE OK
     expect(updatedresponse.ok()).toBeTruthy();
     const patcheddata = (await updatedresponse.json()).data;
     expect(patcheddata).toMatchObject(allFieldsUpdated);
+
+    //PATCH REQ FOR TWO FIELDS OBJ
+    const updatedresponsepartialpatch = await request.patch(
+      `/api/v1/staticpages/${staticPageToUpdate._id}`,
+      {
+        data: twoFieldsUpdated,
+      }
+    );
+
+    expect(updatedresponse.ok()).toBeTruthy();
+    const patchedpartialpatchdata = (await updatedresponsepartialpatch.json())
+      .data;
+    expect(patchedpartialpatchdata).toMatchObject(twoFieldsUpdated);
+
+    //FIELDS DONT EXIST PATCH REQ
+    const fielddoesnotexistresponse = await request.patch(
+      `/api/v1/staticpages/${staticPageToUpdate._id}`,
+      {
+        data: nonExistantFields,
+      }
+    );
+    expect(fielddoesnotexistresponse.ok()).toBeTruthy();
+    const responsejson = await fielddoesnotexistresponse.json();
+    expect(fielddoesnotexistresponse).not.toMatchObject({ allFieldsUpdated });
   });
 
   //SOFT DELETE TESTS
-  test.describe("api/v1/staticpages/[id]", () => {
-    test("returns a static page that has a new field deleted_at", async ({
-      request,
-      db,
-    }) => {
-      //Get a staticpage
-      const staticPageToDelete = await db
-        .collection("staticpages")
-        .findOne({ deleted_at: { $eq: null } });
+  test("returns a static page that has a new field deleted_at", async ({
+    request,
+    db,
+  }) => {
+    //Get a staticpage
+    const staticPageToDelete = await db
+      .collection("staticpages")
+      .findOne({ deleted_at: { $eq: null } });
 
-      //DELETE REQ
-      const deletedResponse = await request.delete(
-        `/api/v1/staticpages/${staticPageToDelete._id}`
-      );
-      //after req, res should be ok no err
-      expect(deletedResponse).toBeOK();
-      const responsejson = await deletedResponse.json();
+    //static page that is deleted_at: null
+    const undeletedStaticPage = {
+      deleted_at: null,
+    };
+    //DELETE REQ
+    const deletedResponse = await request.delete(
+      `/api/v1/staticpages/${staticPageToDelete._id}`
+    );
+    //after req, res should be ok no err
+    expect(deletedResponse).toBeOK();
+    const responsejson = await deletedResponse.json();
 
-      //test for success msg
-      expect(responsejson).toEqual({ message: "success" });
+    //test for success msg
+    expect(responsejson).toEqual({ message: "success" });
 
-      //read db with id and make sure deleted at is not null
-      const staticPagePostDelete = await db
-        .collection("staticpages")
-        .findOne({ _id: new ObjectId(staticPageToDelete._id) });
-      expect(staticPagePostDelete.deleted_at).not.toBeNull();
-    });
+    //read db with id and make sure deleted at is not null
+    const staticPagePostDelete = await db
+      .collection("staticpages")
+      .findOne({ _id: new ObjectId(staticPageToDelete._id) });
+    expect(staticPagePostDelete.deleted_at).not.toBeNull();
+
+    //A test for undeleting a static page []
+    const undeletedResponse = await request.patch(
+      `/api/v1/staticpages/${undeletedStaticPage._id}`
+    );
+    expect(undeletedResponse).toBeNull();
   });
 });
