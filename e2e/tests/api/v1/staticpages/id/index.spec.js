7957fce81324f879400eb89ac813e0616dd16920
badgerbadgers <tim.almacen@gmail.com>
2023-04-20 12:03:18 -0700

    Playwright tests now work, separated 3 PATCH request as their own tests, in previous commit added validators to updateFindByIdAndUpdate method

--- e2e/tests/api/v1/staticpages/id/index.spec.js
+++ e2e/tests/api/v1/staticpages/id/index.spec.js
@@ -1,11 +1,12 @@
 import { test, expect } from "e2e/fixtures/testAsAdmin";
 import { faker } from "@faker-js/faker";
+
 //make smaller tests for each obj
 //make tests expecting data i sent
 const { ObjectId } = require("mongodb");
 test.describe("/api/v1/staticpages/[id]", () => {
   //PATCH TESTS
-  test("returns an updated static page that contains at least one or more updated field(s)", async ({
+  test("returns an updated static page that contains at four updated fields", async ({
     request,
     db,
   }) => {
@@ -23,56 +24,71 @@ test.describe("/api/v1/staticpages/[id]", () => {
       title: faker.lorem.words(2),
     };
 
+    //PATCH REQ FOR ALL FIELDS
+    const updatedresponse = await request.patch(`/api/v1/staticpages/${id}`, {
+      data: allFieldsUpdated,
+    });
+
+    //PATCH RESPONSE OK
+    expect(updatedresponse.ok()).toBeTruthy();
+    const patcheddata = (await updatedresponse.json()).data;
+
+    expect(patcheddata).toMatchObject(allFieldsUpdated);
+  });
+
+  test("returns an updated static page that contains two updated field(s)", async ({
+    request,
+    db,
+  }) => {
+    //Get a staticpage
+    const staticPageToUpdate = await db
+      .collection("staticpages")
+      .findOne({ wordpress_id: { $ne: null } });
+    const id = staticPageToUpdate._id.toString();
+
     //PARTIAL PATCH OBJ
     const twoFieldsUpdated = {
       isShown: faker.datatype.boolean(),
       wordpress_id: faker.datatype.number(10000),
     };
+    //PATCH REQ FOR TWO FIELDS OBJ
+    const updatedresponsepartialpatch = await request.patch(
+      `/api/v1/staticpages/${id}`,
+      {
+        data: twoFieldsUpdated,
+      }
+    );
 
+    expect(updatedresponsepartialpatch.ok()).toBeTruthy();
+    const patchedpartialpatchdata = (await updatedresponsepartialpatch.json())
+      .data;
+    expect(patchedpartialpatchdata).toMatchObject(twoFieldsUpdated);
+  });
+
+  test("returns an undefined variable after a PATCH request with non-existant fields is made", async ({
+    request,
+    db,
+  }) => {
+    //Get a staticpage
+    const staticPageToUpdate = await db
+      .collection("staticpages")
+      .findOne({ wordpress_id: { $ne: null } });
+    const id = staticPageToUpdate._id.toString();
     //NON EXISTANT OBJ FIELDS
     const nonExistantFields = {
       fakeField: "this wont work",
       fakeField2: "this also wont work",
     };
-
-    //PATCH REQ FOR ALL FIELDS
-    const updatedresponse = await request.patch(
-      `/api/v1/staticpages/63e14ad6b8f9d3b00a64dc22`,
-      { data: { allFieldsUpdated } }
-    );
-
-    //PATCH RESPONSE OK
-    expect(updatedresponse.ok()).toBeTruthy();
-    const patcheddata = (await updatedresponse.json()).data;
-    console.log("patched", patcheddata);
-    console.log("allFieldsUpdated", allFieldsUpdated);
-    // expect(patcheddata).toContainEqual(allFieldsUpdated);
-
-    //PATCH REQ FOR TWO FIELDS OBJ
-    const updatedresponsepartialpatch = await request.patch(
-      `/api/v1/staticpages/63e14ad6b8f9d3b00a64dc22`,
-      {
-        data: { twoFieldsUpdated },
-      }
-    );
-
-    expect(updatedresponse.ok()).toBeTruthy();
-    const patchedpartialpatchdata = (await updatedresponsepartialpatch.json())
-      .data;
-    // expect(patchedpartialpatchdata).toMatchObject(twoFieldsUpdated);
-
     //FIELDS DONT EXIST PATCH REQ
     const fielddoesnotexistresponse = await request.patch(
-      `/api/v1/staticpages/63e14ad6b8f9d3b00a64dc22`,
+      `/api/v1/staticpages/${id}`,
       {
         data: nonExistantFields,
       }
     );
     expect(fielddoesnotexistresponse.ok()).toBeTruthy();
-    const responsejson = await fielddoesnotexistresponse.json();
-    //additional tests
-    //make smaller tests
-    expect(responsejson).not.toMatchObject(allFieldsUpdated);
+    const responsejson = await fielddoesnotexistresponse.json().data;
+    expect(responsejson).toBeUndefined();
   });
 
   //SOFT DELETE TESTS
