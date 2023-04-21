a892a0d8552c410f8afc5504bf5a774ac062e632
badgerbadgers <tim.almacen@gmail.com>
2023-04-21 07:35:28 -0700

    tests are now broken up into smaller tests, soft delete assertions for checking deleted_at field after DELETE request has been added

--- e2e/tests/api/v1/staticpages/id/index.spec.js
+++ e2e/tests/api/v1/staticpages/id/index.spec.js
@@ -92,40 +92,60 @@ test.describe("/api/v1/staticpages/[id]", () => {
   });
 
   //SOFT DELETE TESTS
-  test("returns a static page that has a new field deleted_at", async ({
-    request,
-    db,
-  }) => {
+  // test("returns a static page that has a new field deleted_at", async ({
+  //   request,
+  //   db,
+  // }) => {
+  //   //Get a staticpage
+  //   const staticPageToDelete = await db
+  //     .collection("staticpages")
+  //     .findOne({ deleted_at: { $eq: null } });
+
+  //   //static page that is deleted_at: null
+  //   const undeletedStaticPage = {
+  //     deleted_at: null,
+  //   };
+  //   const id = staticPageToDelete._id;
+  //   //DELETE REQ
+  //   const deletedResponse = await request.delete(`/api/v1/staticpages/${id}`);
+  //   //after req, res should be ok no err
+  //   expect(deletedResponse).toBeOK();
+  //   const responsejson = await deletedResponse.json();
+
+  //   //test for success msg
+  //   expect(responsejson).toEqual({ message: "Page has been deleted." });
+
+  //   //read db with id and make sure deleted at is not null
+  //   const staticPagePostDelete = await db
+  //     .collection("staticpages")
+  //     .findOne({ _id: id });
+  //   expect(staticPagePostDelete.deleted_at).not.toBeNull();
+
+  //   //A test for undeleting a static page []
+  //   const undeletedResponse = await request.patch(`/api/v1/staticpages/${id}`);
+  //   expect(undeletedResponse.deleted_at).toBeUndefined();
+  // });
+  test("soft deletes a static page", async ({ request, db }) => {
     //Get a staticpage
     const staticPageToDelete = await db
       .collection("staticpages")
       .findOne({ deleted_at: { $eq: null } });
 
-    //static page that is deleted_at: null
-    const undeletedStaticPage = {
-      deleted_at: null,
-    };
+    const id = staticPageToDelete._id.toString();
     //DELETE REQ
-    const deletedResponse = await request.delete(
-      `/api/v1/staticpages/${staticPageToDelete._id}`
-    );
+    const deletedResponse = await request.delete(`/api/v1/staticpages/${id}`);
     //after req, res should be ok no err
     expect(deletedResponse).toBeOK();
     const responsejson = await deletedResponse.json();
+    expect(responsejson.message).toContain("Page has been deleted.");
 
-    //test for success msg
-    expect(responsejson).toEqual({ message: "Page has been deleted." });
-
-    //read db with id and make sure deleted at is not null
-    const staticPagePostDelete = await db
+    //get document by id and confirm page has been soft deleted
+    const confirmPageDeleted = await db
       .collection("staticpages")
-      .findOne({ _id: new ObjectId(staticPageToDelete._id) });
-    expect(staticPagePostDelete.deleted_at).not.toBeNull();
+      .findOne({ _id: ObjectId(staticPageToDelete._id) });
 
-    //A test for undeleting a static page []
-    const undeletedResponse = await request.patch(
-      `/api/v1/staticpages/${undeletedStaticPage._id}`
-    );
-    expect(undeletedResponse.deleted_at).toBeUndefined();
+    //assertions to check deleted_at is not null
+    expect(confirmPageDeleted.deleted_at).not.toBeNull();
+    expect(confirmPageDeleted.deleted_at).toBeDefined();
   });
 });
