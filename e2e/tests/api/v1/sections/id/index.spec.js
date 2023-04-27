0f4964d9c3ec3ad6734ae4788e20c496ec513c51
badgerbadgers <tim.almacen@gmail.com>
2023-04-27 13:44:10 -0700

    created patch tests for sections

--- e2e/tests/api/v1/sections/id/index.spec.js
+++ e2e/tests/api/v1/sections/id/index.spec.js
@@ -1,9 +1,42 @@
 import { test, expect } from "e2e/fixtures/testAsAdmin";
+import { faker } from "@faker-js/faker";
+import { ObjectID } from "mongodb";
 
 test.describe("/api/v1/sections/{id}", () => {
   //PATCH TESTS
-  test("tests a patched document in the database", async ({
-    request,
-    db,
-  }) => {});
+  test("tests a patched document in the database", async ({ request, db }) => {
+    //get section
+    const sectionToPatch = await db
+      .collection("sections")
+      .findOne({ _id: { $ne: null } });
+    const id = sectionToPatch._id.toString();
+    //create data to send for patch req
+    const patchedSectionObj = {
+      _id: id,
+      title: faker.lorem.words(5),
+      order: faker.datatype.number(10),
+      course: faker.database.mongodbObjectId(),
+    };
+    //PATCH req
+    const response = await request.patch(`/api/v1/sections/${id}`, {
+      data: patchedSectionObj,
+    });
+
+    expect(response.ok()).toBeTruthy();
+    const updatedsection = (await response.json()).data;
+
+    expect(updatedsection).toMatchObject(patchedSectionObj);
+    expect(updatedsection).toHaveProperty("title");
+    expect(updatedsection).toHaveProperty("course");
+    expect(updatedsection).toHaveProperty("_id");
+    expect(updatedsection).toHaveProperty("order");
+
+    //call db get patched document, then check if document was patched
+    const patchedSection = await db
+      .collection("sections")
+      .findOne({ _id: sectionToPatch._id });
+
+    expect(patchedSection.title).toMatch(patchedSectionObj.title);
+    expect(patchedSection.order).toBe(patchedSectionObj.order);
+  });
 });
