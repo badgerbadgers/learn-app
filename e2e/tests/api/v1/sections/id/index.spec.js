28e2b9c2389b7c2c74f47ce8441da86c3c01ccef
badgerbadgers <tim.almacen@gmail.com>
2023-04-27 11:45:32 -0700

    created new test file for v1/sections/id/index.spec.js along with boilerplate

---
+++ e2e/tests/api/v1/sections/id/index.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from "e2e/fixtures/testAsAdmin";
+
+test.describe("/api/v1/sections/{id}", () => {
+  //PATCH TESTS
+  test("tests a patched document in the database", async ({
+    request,
+    db,
+  }) => {});
+});
