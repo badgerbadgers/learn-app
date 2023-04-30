8a72b91fa59ac49ec8c94d0bcbfdc26b7b79c7d3
OxiBo <xenos2000@ukr.net>
2023-04-29 23:08:00 -0400

    Finish PATCH and DELETE for api/v1/lessons/[id]

--- pages/api/v1/lessons/[id]/index.js
+++ pages/api/v1/lessons/[id]/index.js
@@ -69,8 +69,8 @@
  *             section:
  *                 type: string
  *             deleted_at:
- *                 type: ???
- *           example: {"title": "JavaScript Arrays", "order": 3.2, "submission_link": { "label": "Submit Assignment", "url": "https://airtable.com/shrF8xGZowU5HEuIJ?prefill_Assignment%20Title=HTML Basics"},"learning_objectives": ["Learn everything", "Learn JavaScript arrays"], "mindset_content": "For many, failure has become deeply engrained in us as something to avoid. And for that reason, as you’re learning to code, you may find that your gut response to bright red error messages or long stack traces is one of fear, anger, or frustration. But we’ll let you in on a little secret—those error messages and stack traces are a gift.", "materials": ["62e26db569dd077fc82fbfce","62e26dbb69dd077fc82fbfed"], "assignments": ["62e26dc669dd077fc82fc002", "62e26dc569dd077fc82fbff4"], "section": "633d9916ec0d4b5e83a6b062"}
+ *                 type: 'null'
+ *           example: {"title": "JavaScript Arrays", "order": 3.2, "submission_link": { "label": "Submit Assignment", "url": "https://airtable.com/shrF8xGZowU5HEuIJ?prefill_Assignment%20Title=HTML Basics"},"learning_objectives": ["Learn everything", "Learn JavaScript arrays"], "mindset_content": "For many, failure has become deeply engrained in us as something to avoid. And for that reason, as you’re learning to code, you may find that your gut response to bright red error messages or long stack traces is one of fear, anger, or frustration. But we’ll let you in on a little secret—those error messages and stack traces are a gift.", "materials": ["62e26db569dd077fc82fbfce","62e26dbb69dd077fc82fbfed"], "assignments": ["62e26dc669dd077fc82fc002", "62e26dc569dd077fc82fbff4"], "section": "633d9916ec0d4b5e83a6b062", deleted_at: null}
  *     responses:
  *       200:
  *         description: Provides a lesson's data
@@ -100,7 +100,10 @@
 
 // TODO - swagger - change description if anything returns from DELETED request
 import Lesson from "lib/models/Lesson";
+import Assignment from "lib/models/Assignment";
 import dbConnect from "lib/dbConnect";
+import Material from "lib/models/Material";
+import Section from "lib/models/Section";
 
 export default async function handler(req, res) {
   const { method } = req;
@@ -159,70 +162,99 @@ export const getLesson = async (id) => {
   return data;
 };
 
-// export const updateCourse = async (id, updates) => {
-//   // filter updates to extract allowed fields to perform update
-//   const allowedFields = ["course_name", "lessons", "deleted_at"];
-//   const filteredUpdates = allowedFields.reduce((fields, current) => {
-//     if (current === "deleted_at" && updates[current] === null) {
-//       return { ...fields, [current]: updates[current] };
-//     }
-//     if (current === "deleted_at" && updates[current]) {
-//       // check if deleted_at is a Date not null and do not let the field to go to updates (deleted not allowed in PATCH)
-//       return fields;
-//     }
-//     if (updates[current]) {
-//       return { ...fields, [current]: updates[current] };
-//     }
-//     return fields;
-//   }, {});
+// make sure provided ids exist in model db
+const foundInDataBase = async (model, data) => {
+  // find which of the provided ids exist in model database, if not - throw an error
+  const idList = await model.find({ _id: { $in: [...data] } }, "_id");
+  // throw an error if not each id provided is found in db
+  if (!idList.length || data.length !== idList.length) {
+    throw new Error(
+      `All ${model.modelName} ids provided must be unique and exist in the data base`
+    );
+  }
+  return idList.map((item) => item._id);
+};
 
-//   await dbConnect();
-//   if (filteredUpdates.course_name) {
-//     //make sure course_name is unique
-//     const duplicateCourseName = await Course.findOne({
-//       course_name: filteredUpdates.course_name,
-//     });
-//     if (duplicateCourseName) {
-//       throw new Error("Duplicate Course Name");
-//     }
-//   }
+export const updateLesson = async (id, updates) => {
+  // filter updates to extract allowed fields to perform update
+  const allowedFields = [
+    "title",
+    "order",
+    "submission_link",
+    "learning_objectives",
+    "mindset_content",
+    "materials",
+    "assignments",
+    "section",
+    "deleted_at",
+  ];
+  const filteredUpdates = allowedFields.reduce((fields, current) => {
+    if (current === "deleted_at" && updates[current] === null) {
+      return { ...fields, [current]: updates[current] };
+    }
+    if (current === "deleted_at" && updates[current]) {
+      // check if deleted_at is a Date not null and do not let delete a lesson in PATCH )
+      throw new Error("Not allowed to delete a lesson via PATCH method");
+      //  return fields;
+    }
+    if (
+      current === "submission_link" &&
+      updates[current] &&
+      (!updates[current].label || !updates[current].url)
+    ) {
+      throw new Error("Submission link must include label and valid url");
+    }
+    if (updates[current]) {
+      return { ...fields, [current]: updates[current] };
+    }
+    return fields;
+  }, {});
 
-//   // make sure provided lessons exist in db
-//   if (filteredUpdates.lessons) {
-//     // find which of the provided lessons exist in lessons database, if not - throw an error
-//     const lessons = await Lesson.find(
-//       {
-//         _id: { $in: filteredUpdates.lessons },
-//       },
-//       "_id"
-//     );
+  if (!filteredUpdates.title) {
+    throw new Error("Title is required to update a lesson");
+  }
 
-//     // throw an error if not each lesson id provided is found in db
-//     if (!lessons.length || filteredUpdates.lessons.length !== lessons.length) {
-//       throw new Error(
-//         "All lessons ids provided must be unique and exist in the data base"
-//       );
-//     }
-//     filteredUpdates.lessons = lessons;
-//   }
-//   // since an error is returned if filteredUpdates is an empty object because database won't perform an update with empty object provided, return error if there are no valid fields to update
-//   if (Object.keys(filteredUpdates).length === 0) {
-//     throw new Error(
-//       "Valid data to perform an update for the course not provided"
-//     );
-//   }
-//   const updatedCourse = await Course.findByIdAndUpdate(id, filteredUpdates, {
-//     new: true,
-//     runValidators: true,
-//   });
+  await dbConnect();
 
-//   if (!updatedCourse) {
-//     const error = new Error();
-//     error.status = 404;
-//     error.message = `Could not find and update course with id - ${id}`;
-//     throw error;
-//   }
-//   const updatedCoursePopulate = await updatedCourse.populate("lessons");
+  if (filteredUpdates.materials) {
+    filteredUpdates.materials = await foundInDataBase(
+      Material,
+      filteredUpdates.materials
+    );
+  }
 
-//   return updatedCoursePopulate;
-// };
+  if (filteredUpdates.section) {
+    const [section] = await foundInDataBase(Section, [filteredUpdates.section]);
+    filteredUpdates.section = section;
+  }
+
+  if (filteredUpdates.assignments) {
+    filteredUpdates.assignments = await foundInDataBase(
+      Assignment,
+      filteredUpdates.assignments
+    );
+  }
+
+  // return error if there are no valid fields to update
+  if (Object.keys(filteredUpdates).length === 0) {
+    throw new Error(
+      "Valid data to perform an update for the course not provided"
+    );
+  }
+  const updatedLesson = await Lesson.findByIdAndUpdate(id, filteredUpdates, {
+    new: true,
+    runValidators: true,
+  });
+  if (!updatedLesson) {
+    const error = new Error();
+    error.status = 404;
+    error.message = `Could not find and update lesson with id - ${id}`;
+    throw error;
+  }
+  await updatedLesson.save();
+  const updatedLessonPopulate = await updatedLesson.populate(
+    "materials assignments section"
+  );
+
+  return updatedLessonPopulate;
+};
