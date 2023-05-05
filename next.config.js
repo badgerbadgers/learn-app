7c754a775a56ba757d3c2ce959a3e2c21612ce28
badgerbadgers <tim.almacen@gmail.com>
2023-05-05 11:12:57 -0700

    changed wordpressUrl in next.config.js file

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     swSrc: "service-worker.js",
   },
   env: {
-    wordpressUrl: `https://learn.codethedream.org/wp-json/wp/v2/pages/`,
+    wordpressUrl: `https://learn.codethedream.org/wp-json/wp/v2/pages?parent=378`,
   },
 };
 
