fc29dbe7e504743ff499df52ce2a2a09543329ce
OxiBo <xenos2000@ukr.net>
2023-05-12 21:31:32 -0400

    Improve data structure returned from getServerSideProps

--- pages/admin/cohorts/[id].js
+++ pages/admin/cohorts/[id].js
@@ -1,6 +1,5 @@
 import { Box, Button, Container, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-
 import CohortHeader from "./components/CohortHeader";
 import Grid from "@mui/material/Unstable_Grid2";
 import IndCohortGrid from "./components/IndCohortGrid";
@@ -9,60 +8,14 @@ import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { privateLayout } from "../../../components/layout/PrivateLayout";
 import { format, formatDistance } from "date-fns";
-
 import ScheduleModal from "./components/ScheduleModal";
 import { getCohortById } from "pages/api/v1/cohorts/[id]";
 import { getCohortStudents } from "pages/api/v1/cohorts/[id]/students";
 
 const IndividualCohortPage = ({ cohort, students, prevCohort, nextCohort }) => {
-  // console.log(cohort);
-  // console.log(students);
-
   const [open, setOpen] = useState(false);
-  //const [schedule, setSchedule] = useState(cohort.schedule);
   const [startDate, setStartDate] = useState(cohort.start_date);
 
-  // const router = useRouter();
-  // const query = router.query;
-
-  // useEffect(() => {
-  //   // let cohort = {};
-
-  //   // const url = `/api/v1/cohorts/${query.id}`;
-  //   // const params = {}; //{ slug: query.slug };
-  //   try {
-  //     (async () => {
-  //       setLoading(true);
-  //       const response = await getData(params, url);
-  //       cohort = response.data;
-  //       setCohort(cohort);
-  //       console.log(cohort);
-  //       setSchedule(cohort.schedule);
-  //       setLoading(false);
-  //       //TMP for MVP, needs to be changed when DB schema and Student model are ready
-  //       if (cohort.students) {
-  //         const localStudents = [];
-  //         cohort.students.map((st) => {
-  //           if (!st.user) {
-  //             return;
-  //           }
-  //           const student = {
-  //             id: st.user._id,
-  //             ...st.user,
-  //             studentAdded: st.added_at,
-  //           };
-  //           delete student._id;
-  //           console.log(student);
-  //           localStudents.push(student);
-  //         });
-  //         await setStudents(localStudents);
-  //       }
-  //     })();
-  //   } catch (error) {
-  //     console.log("An error from getData in", url, error);
-  //   }
-  // }, [query]);
-  //  {!cohort && !loading && (
   return (
     <Container>
       {!cohort && (
@@ -81,7 +34,6 @@ const IndividualCohortPage = ({ cohort, students, prevCohort, nextCohort }) => {
             title={cohort.cohort_name}
             course={cohort.course}
             setOpen={setOpen}
-            //   startDate={cohort.start_date}
             startDate={startDate}
             setStartDate={setStartDate}
             scheduleLen={cohort.schedule && cohort.schedule.length} // TODO - check if schedule is not null?
@@ -97,8 +49,6 @@ const IndividualCohortPage = ({ cohort, students, prevCohort, nextCohort }) => {
             cohortName={cohort.cohort_name}
             startDate={cohort.start_date}
             setStartDate={setStartDate}
-            // schedule={schedule}
-            // setSchedule={setSchedule}
           />
         </Box>
       )}
@@ -112,7 +62,7 @@ IndividualCohortPage.getLayout = privateLayout;
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
-  // TODO - check if the logged in user is admin
+  // TODO - check if the logged in user is admin?
 
   if (!session) {
     return {
@@ -126,25 +76,33 @@ export async function getServerSideProps(context) {
 
   try {
     const cohortId = context.params.id;
-    const [prevCohort, nextCohort] =
-      (await getPrevAndNextCohortSlugs(cohortId)) || null; // TODO  - why return null if nothing is returned
+
     // get cohort data
     const cohort = JSON.parse(JSON.stringify(await getCohortById(cohortId)));
     // get cohort students
     const data = JSON.parse(JSON.stringify(await getCohortStudents(cohortId)));
+    console.log("----------");
+    console.log(data, "DATA");
+    // TODO  - the filtering should be done in API handler
     const students =
-      data.students?.map((st) => {
-        if (st.user) {
-          const filteredStudent = {
-            id: st.user._id,
-            ...st.user,
-            added_at: st.added_at,
-          };
-          delete filteredStudent._id;
-          return filteredStudent;
-        }
-      }) || [];
-
+      data.students
+        ?.map((st) => {
+          if (st.user) {
+            const filteredStudent = {
+              id: st.user._id,
+              ...st.user,
+              added_at: st.added_at, // add '|| null' here
+            };
+            delete filteredStudent._id;
+            return filteredStudent;
+          }
+        })
+        .filter((student) => student) || []; // filter to get students which are not null and return empty array if students returned as empty array
+    console.log(students, "++++++++");
+    const [prevCohort, nextCohort] = (await getPrevAndNextCohortSlugs(
+      cohortId
+    )) || [null, null]; // || null; // ** null is not iterable
+    // https://avatars.githubusercontent.com/u/2739321?v=4
     return {
       props: {
         cohort,
@@ -153,8 +111,15 @@ export async function getServerSideProps(context) {
         nextCohort,
       },
     };
-  } catch (err) {
-    console.error(err);
-    // TODO - what return here: redirect to a custom error page or send an error in props and let components handle it???
+  } catch (error) {
+    console.error(error);
+    // TODO - what return here: redirect to a custom error page (404, 403, 500) or send an error in props and let components handle it???
+
+    // nextjs docs - https://nextjs.org/blog/next-10#notfound-support
+    if (error.status === 404) {
+      return { notFound: true };
+    }
+    // possible solution ideas - https://github.com/vercel/next.js/discussions/12652#discussioncomment-90145
+    throw new Error(error);
   }
 }
