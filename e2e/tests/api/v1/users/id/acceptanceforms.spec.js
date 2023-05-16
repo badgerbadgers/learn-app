1e4be3a5cb56a0d6bd038ec2fa269a22fadfea9d
Oxana Ermolenko <oka@Oxanas-Mac-mini.local>
2023-05-16 07:26:50 -0700

    refactor GET to remove 404 error and move try catch

--- e2e/tests/api/v1/users/id/acceptanceforms.spec.js
+++ e2e/tests/api/v1/users/id/acceptanceforms.spec.js
@@ -5,35 +5,38 @@ import { ObjectId } from "bson";
 test.describe("/api/v1/users/[id]/acceptanceforms", () => {
   //GET TESTS
 
-  test(
-    "returns all acceptanceforms for user by session",
-    async ({ request, db }) => {
-      //create acceptanceform for session user
-      const userId = "62b22b42f4da59dbea98071b";
-        
-
-      //call GET and get all acceptance form for sessin's user
-      const response = await request.get(`/api/v1/users/${userId}/acceptanceforms`);
-      expect(response.ok()).toBeTruthy();
-
-      const acceptanceforms = (await response.json()).data;
-
-    }
-  );
-
-  test.fixme("get returns 404 if acceptanceforms not found", async ({ request, db }) => {
-    //call GET and get all acceptanceforms by user Id
+  test("returns all acceptanceforms", async ({ request }) => {
     const userId = "62b22b42f4da59dbea98071b";
-    //delete created before acceptance form
-    await db
-      .collection("acceptanceforms")
-      .deleteOne({ user: ObjectId(userId) });
     const response = await request.get(
       `/api/v1/users/${userId}/acceptanceforms`
     );
-    expect(response.ok()).toBeFalsy();
-    expect(response.status()).toBe(404);
+    expect(response.ok()).toBeTruthy();
+    const acceptanceforms = (await response.json()).data;
+    expect(acceptanceforms.length).toBeGreaterThan(0);
   });
+
+  test.fixme("returns more than one acceptanceforms", async ({ request }) => {
+    const userId = "62b22b42f4da59dbea98071b";
+    const response = await request.get(
+      `/api/v1/users/${userId}/acceptanceforms`
+    );
+    expect(response.ok()).toBeTruthy();
+    const acceptanceforms = (await response.json()).data;
+    expect(acceptanceforms.length).toBeGreaterThan(1);
+  });
+
+  test.fixme(
+    "get returns 400 if acceptanceforms not found",
+    async ({ request }) => {
+      const userId = "62b22b42f4da59dbea98071b";
+
+      const response = await request.get(
+        `/api/v1/users/${userId}/acceptanceforms`
+      );
+      expect(response.ok()).toBeFalsy();
+      expect(response.status()).toBe(400);
+    }
+  );
 
   test(
     "get returns 400 if userId not found",
--- pages/api/v1/users/[id]/acceptanceforms.js
+++ pages/api/v1/users/[id]/acceptanceforms.js
@@ -9,8 +9,6 @@
  *         description: Get all acceptanceforms by user id
  *       400:
  *         description: Error messages
- *       404:
- *         description: No acceptanceforms found
  */
 
 import AcceptanceForm from "lib/models/AcceptanceForm";
@@ -21,33 +19,32 @@ const { ObjectId } = require("mongodb");
 export default async function handler(req, res) {
   const { method } = req;
   const { id } = req.query;
-  switch (method) {
-    case "GET":
-      try {
+  try {
+    switch (method) {
+      case "GET":
         //Get acceptanceforms for user by Id
         const acceptanceforms = await getAcceptanceforms(id);
-        if (!acceptanceforms) {
-          res.status(404).json({
-            message: "No acceptanceforms found",
-          });
-          return;
-        }
         res.status(200).json({ data: acceptanceforms });
         return;
-      } catch (error) {
-        res.status(error.status || 400).json({
-          message: error.message,
-        });
-        return;
-      }
-    default:
-      res.setHeader("Allow", ["GET"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
+
+      default:
+        res.setHeader("Allow", ["GET"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(error.status || 400).json({
+      message: error.message,
+    });
+    return;
   }
 }
 
 export const getAcceptanceforms = async (id) => {
   await dbConnect();
   const acceptanceforms = await AcceptanceForm.find({user: ObjectId(id)});
+    if (!acceptanceforms) {
+      return null;
+    }
   return acceptanceforms;
 };
