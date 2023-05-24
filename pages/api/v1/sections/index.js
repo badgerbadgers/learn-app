c22aa9ef70fda0263a6324ad2e2f2927356cd97c
badgerbadgers <tim.almacen@gmail.com>
2023-05-24 09:58:28 -0700

    refactored /api/v1/sections/index with a single try catch block, updated swagger api documentation, an exported function that now returns null if no documents are found in db

--- pages/api/v1/sections/index.js
+++ pages/api/v1/sections/index.js
@@ -30,7 +30,13 @@
  *                       course:
  *                         type: number
  *                         example: 75ge23c42f9b73c474826693
- *
+ *                       deleted_at:
+ *                         type: date
+ *                         example: 2023-05-05T01:31:37.035+00:00
+ *       400:
+ *         description: Error messages
+ *       404:
+ *         description: Error message if no sections are found
  */
 
 import Section from "lib/models/Section";
@@ -39,29 +45,34 @@ import dbConnect from "lib/dbConnect";
 export default async function handler(req, res) {
   const { method } = req;
 
-  switch (method) {
-    case "GET":
-      try {
+  try {
+    switch (method) {
+      case "GET":
         const sections = await getSections();
+        if (!sections) {
+          const error = new Error();
+          error.status = 404;
+          error.message = `Could not find any sections`;
+          throw error;
+        }
         res.status(200).json({ data: sections });
-        return; 
-      } catch (error) {
-        res.status(400).json({ message: error.message });
         return;
-      }
-    default:
-      res.setheader("Allow", ["GET"]);
-      res.status(405).end(`Method ${method} Not Allowed`);
+      default:
+        res.setheader("Allow", ["GET"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(error.status || 400).json({ message: error.message });
   }
 }
 
 export const getSections = async () => {
-  try {
-    await dbConnect();
+  await dbConnect();
 
-    const allsections = await Section.find({});
-    return allsections;
-  } catch (error) {
-    throw new Error("Error getting sections");
+  const allsections = await Section.find({});
+  if (!allsections) {
+    return null;
   }
+  return allsections;
 };
