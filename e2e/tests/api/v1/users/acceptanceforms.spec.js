1546c2f20f75e62440fd93a82200a8904a9c7313
Oxana Ermolenko <oka@Oxanas-Mac-mini.local>
2023-05-24 21:33:11 -0700

    added test that get not to match first acceptanceform

--- e2e/tests/api/v1/users/acceptanceforms.spec.js
+++ e2e/tests/api/v1/users/acceptanceforms.spec.js
@@ -49,6 +49,53 @@ const newAcceptanceform = {
   personal_phone: faker.phone.number(),
   completed_at: faker.date.recent().toISOString(),
 };
+const secondAcceptanceform = {
+  __v: 0,
+  active_step: 0,
+  address_USResident: "no",
+  address_mailing_city: faker.address.cityName(),
+  address_mailing_country: faker.address.country(),
+  address_mailing_same: false,
+  address_mailing_state: faker.address.state(),
+  address_mailing_street1: faker.address.streetAddress(),
+  address_mailing_street2: "",
+  address_mailing_zipcode: faker.address.zipCode(),
+  address_physical_city: faker.address.cityName(),
+  address_physical_country: faker.address.country(),
+  address_physical_state: faker.address.state(),
+  address_physical_street1: faker.address.streetAddress(),
+  address_physical_street2: "",
+  address_physical_zipcode: faker.address.zipCode(),
+  background_learning_style: [],
+  background_prior_coding_education: "",
+  background_prior_coding_languages: [],
+  consent_leave_notice: true,
+  consent_work_commitment: true,
+  demographics_dob: faker.date.birthdate().toISOString(),
+  demographics_education: "",
+  demographics_employed: "",
+  demographics_gender_identity: ["Other"],
+  demographics_gender_identity_self: "test",
+  demographics_in_school: "",
+  demographics_low_income: "no",
+  demographics_pronouns: "",
+  demographics_race_ethnicity: "Other",
+  demographics_race_ethnicity_self: "test",
+  demographics_spoken_languages: "",
+  emergency_contact1_name: faker.name.fullName(),
+  emergency_contact1_phone: faker.phone.number(),
+  emergency_contact1_relationship: "Parent/Mother/Father",
+  emergency_contact2_name: faker.name.fullName(),
+  emergency_contact2_phone: faker.phone.number(),
+  emergency_contact2_relationship: "Friend",
+  is_completed: true,
+  personal_email: faker.internet.email(),
+  personal_first_name: faker.name.firstName(),
+  personal_github: faker.random.alphaNumeric(10),
+  personal_last_name: faker.name.lastName(),
+  personal_phone: faker.phone.number(),
+  completed_at: faker.date.recent().toISOString(),
+};
 
 test.describe("/api/v1/users/acceptanceforms", () => {
   //GET TESTS
@@ -121,14 +168,14 @@ test.describe("/api/v1/users/acceptanceforms", () => {
     const postAcceptanceformLast = await request.post(
       `/api/v1/acceptanceforms`,
       {
-        data: newAcceptanceform,
+        data: secondAcceptanceform,
       }
     );
     expect(postAcceptanceformFirst.ok()).toBeTruthy();
 
     const responsePostAcceptanceformLast = (await postAcceptanceformLast.json())
       .data;
-    expect(responsePostAcceptanceformLast).toMatchObject(newAcceptanceform);
+    expect(responsePostAcceptanceformLast).toMatchObject(secondAcceptanceform);
 
     //check if the user id is the same
     expect(responsePostAcceptanceformLast.user).toEqual(user._id + "");
@@ -140,6 +187,56 @@ test.describe("/api/v1/users/acceptanceforms", () => {
     //check if get response match to last acceptanceform
     const getAcceptanceforms = (await response.json()).data;
     expect(getAcceptanceforms).toMatchObject(responsePostAcceptanceformLast);
+    // delete created before acceptance form
+    await db.collection("acceptanceforms").deleteOne({ user: user });
+  });
+
+  test("not returns first acceptanceforms for user by session", async ({
+    request,
+    user,
+    db,
+  }) => {
+    //create acceptanceform for session user
+    const postAcceptanceformFirst = await request.post(
+      `/api/v1/acceptanceforms`,
+      {
+        data: newAcceptanceform,
+      }
+    );
+    expect(postAcceptanceformFirst.ok()).toBeTruthy();
+
+    //check if post response match to newAcceptanceform
+    const responsePostAcceptanceformFirst = (
+      await postAcceptanceformFirst.json()
+    ).data;
+    expect(responsePostAcceptanceformFirst).toMatchObject(newAcceptanceform);
+
+    //check if the user id is the same
+    expect(responsePostAcceptanceformFirst.user).toEqual(user._id + "");
+
+    //create second acceptanceform for session user
+    const postAcceptanceformLast = await request.post(
+      `/api/v1/acceptanceforms`,
+      {
+        data: secondAcceptanceform,
+      }
+    );
+    expect(postAcceptanceformFirst.ok()).toBeTruthy();
+
+    const responsePostAcceptanceformLast = (await postAcceptanceformLast.json())
+      .data;
+    expect(responsePostAcceptanceformLast).toMatchObject(secondAcceptanceform);
+
+    //check if the user id is the same
+    expect(responsePostAcceptanceformLast.user).toEqual(user._id + "");
+
+    //call GET and get all acceptance form for sessin's user
+    const response = await request.get(`/api/v1/users/acceptanceforms`);
+    expect(response.ok()).toBeTruthy();
+
+    //check if get response not match to first acceptanceform
+    const getAcceptanceforms = (await response.json()).data;
+    expect(getAcceptanceforms).not.toMatchObject(responsePostAcceptanceformFirst);
     // delete created before acceptance form
     await db.collection("acceptanceforms").deleteOne({ user: user });
   });
